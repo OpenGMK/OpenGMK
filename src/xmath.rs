5c0b95fa1aef77323e8ca4a4dd3b401a4a93dcb2
viri <not.viri@protonmail.ch>
2019-02-25 09:31:36 -0600

    xmath: implement `round` in x86 asm, improve test

--- src/xmath.rs
+++ src/xmath.rs
@@ -1,8 +1,30 @@
 // xmath: Contains unconventional math functions that don't behave as you'd expect in GameMaker 8.
-// * These could be optimized with some clever usage of asm!()
 
-/// Mimics the FPU Integer STore (FIST) instruction - rounds to nearest even number.
 #[inline(always)]
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+/// Direct binding to the FPU Integer STore (FIST) instruction - rounds to nearest even number.
+pub fn round(val: f64) -> i32 {
+    let val = val as f32;
+    let out: i32;
+    unsafe {
+        asm!(
+            "sub $$0x04, %rsp   # allocate 4 bytes (x)
+            mov ${0}, (%rsp)    # val -> x
+            flds (%rsp)         # x -> ST(0)
+            fistpl (%rsp)       # FIST(ST(0)) -> x, pop ST(0)
+            movl (%rsp), $0     # x -> out
+            add $$0x04, %rsp"
+
+            : "=r"(out)
+            : "r"(val)
+        );
+    }
+    out
+}
+
+#[inline(always)]
+#[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+/// Mimics the FPU Integer STore (FIST) instruction - rounds to nearest even number.
 pub fn round(val: f64) -> i32 {
     let floor = val.floor();
     let floori = floor as i32;
@@ -24,5 +46,8 @@ mod tests {
         assert_eq!(super::round(1.5), 2);
         assert_eq!(super::round(2.5), 2);
         assert_eq!(super::round(3.5), 4);
+        for i in 0..1000 {
+            assert!(super::round(i as f64 + 0.5) % 2 == 0);
+        }
     }
 }
