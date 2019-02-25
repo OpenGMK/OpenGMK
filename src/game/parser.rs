6bcb6a237c84d2af086ddb8f5561510b3431f620
viri <not.viri@protonmail.ch>
2019-02-25 11:22:46 -0600

    game: create module, start .exe parser

---
+++ src/game/parser.rs
@@ -0,0 +1,5 @@
+use super::Game;
+
+impl Game {
+    pub fn from_exe(mut _exe: Vec<u8>) {}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
-pub(crate) mod xmath;
+#![feature(asm)] // Usage: xmath
 
-fn main() {
-    let val = xmath::round(3.5);
-    println!("Result: {}", val);
+mod game;
+mod xmath;
+
+use crate::game::Game;
+use std::env;
+use std::error::Error;
+use std::fs;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: gm8emu <exe_path>");
+        return Ok(());
+    }
+
+    let data = fs::read(&args[1])?;
+    let _game = Game::from_exe(data);
+
+    Ok(())
 }
