00bc1568a175f62ea98059ac58e58143902c71ad
viri <not.viri@protonmail.ch>
2019-02-27 12:05:13 -0600

    assets: create module, add Sound type

---
+++ src/assets.rs
@@ -0,0 +1,27 @@
+#[derive(Debug)]
+pub struct Sound {
+    /// Asset name
+    pub name: String,
+
+    /// Any of: normal, background, 3d, use multimedia player
+    /// I should make this an enum eventually. TODO
+    pub kind: u32,
+
+    pub file_type: String,
+    pub file_name: String,
+
+    /// This is optional because the associated data doesn't need to exist.
+    /// Fantastic.
+    pub file_data: Option<Box<[u8]>>,
+
+    /// Volume - Between 0 and 1, although the editor only allows as low as 0.3
+    pub volume: f64,
+
+    /// 3D Pan - Between -1 and 1 (L <-> R)
+    pub pan: f64,
+
+    /// TODO: I have no idea what this does.
+    pub preload: bool,
+
+    pub version: u32,
+}
