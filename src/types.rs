53b70a3a026ec30296f619ad53d146845434a771
viri <not.viri@protonmail.ch>
2019-02-28 21:25:36 -0600

    types: create mod, add MANY TYPES (extended msg)
    + added BoundingBox, CollisionMap, Point, Rectangle, Version

---
+++ src/types.rs
@@ -0,0 +1,28 @@
+pub struct BoundingBox {
+    pub width: u32,
+    pub height: u32,
+
+    pub top: u32,
+    pub bottom: u32,
+    pub left: u32,
+    pub right: u32,
+}
+
+pub struct CollisionMap {
+    pub bounds: BoundingBox,
+    pub data: Box<[u8]>,
+    pub version: Version,
+}
+
+#[derive(Debug)]
+pub struct Point {
+    pub x: u32,
+    pub y: u32,
+}
+
+pub struct Rectangle {
+    pub width: u32,
+    pub height: u32,
+}
+
+pub type Version = u32;
