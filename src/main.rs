24b486a219e8162334dcf2eb3b6d6d11979d6e91
viri <not.viri@protonmail.ch>
2019-02-28 23:10:53 -0600

    main: improved argument parser
    * as much as I don't plan on having many CLI options, it's still nice.

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,38 @@ use std::error::Error;
 use std::fs;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().skip(1).collect();
 
-    if args.len() < 2 {
+    if args.len() < 1 {
         println!("Usage: gm8emu <exe_path> [--verbose]");
         return Ok(());
     }
 
-    let verbose = if let Some(arg) = args.get(2) {
-        arg == "--verbose"
-    } else {
-        false
-    };
-    let data = fs::read(&args[1])?;
-    let _game = Game::from_exe(data, verbose)?;
+    let mut verbose = false;
+    let mut path: Option<&String> = None;
+    for arg in args.iter() {
+        match arg.as_ref() {
+            "--verbose" => verbose = true,
+            _ => {
+                if let Some(path) = &path {
+                    println!("Can't open multiple games at once! ({} and {})", path, arg);
+                    std::process::exit(1);
+                } else {
+                    path = Some(arg);
+                }
+            },
+        }
+    }
+
+    if let Some(path) = path {
+        let data = fs::read(path)?;
+        let game = Game::from_exe(data, verbose);
+
+        match game {
+            Ok(_) => println!("Parsing OK!"),
+            Err(err) => return Err(Box::new(err)),
+        }
+    }
 
     Ok(())
 }
