6bf92dcce99cd5811ac9b9a474eb3adebe8d2706
viri <not.viri@protonmail.ch>
2019-03-03 18:52:24 -0600

    bytes: add writers for numbers and strings

--- src/bytes.rs
+++ src/bytes.rs
@@ -1,4 +1,4 @@
-// A lightweight Rust 1.32 replacement for `byteorder` functions.
+// A lightweight Rust 1.32 replacement to byteorder-style functions for std::io Read/Write.
 
 use std::io;
 
@@ -24,9 +24,24 @@ pub trait ReadBytes: io::Read {
     }
 }
 
+pub trait WriteBytes: io::Write {
+    /// Writes a `u32` (little-endian) to the underlying writer.
+    #[inline(always)]
+    fn write_u32_le(&mut self, n: u32) -> io::Result<usize> {
+        Ok(self.write(&n.to_le_bytes())?)
+    }
+
+    /// Reads an `f64` (little-endian) from the underlying writer.
+    #[inline(always)]
+    fn write_f64_le(&mut self, n: f64) -> io::Result<usize> {
+        Ok(self.write(&(n.to_bits()).to_le_bytes())?)
+    }
+}
+
 /// Helper trait for reading strings.
 pub trait ReadString: io::Read {
     /// Reads a pascal-style string from the underlying reader.
+    /// A preceding little-endian u32 indicating size is assumed.
     #[inline(always)]
     fn read_pas_string(&mut self) -> io::Result<String> {
         let len = self.read_u32_le()? as usize;
@@ -36,5 +51,17 @@ pub trait ReadString: io::Read {
     }
 }
 
+pub trait WriteString: io::Write {
+    /// Writes a pascal-style string to the underlying writer.
+    /// A preceding little-endian u32 indicating size will be included.
+    /// Returns the bytes written, including the size prefix.
+    #[inline(always)]
+    fn write_pas_string(&mut self, s: &str) -> io::Result<usize> {
+        Ok(self.write(&(s.len() as u32).to_le_bytes())? + self.write(s.as_bytes())?)
+    }
+}
+
 impl<R> ReadBytes for R where R: io::Read + ?Sized {}
 impl<R> ReadString for R where R: io::Read + ?Sized {}
+impl<W> WriteBytes for W where W: io::Write + ?Sized {}
+impl<W> WriteString for W where W: io::Write + ?Sized {}
