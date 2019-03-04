6b63dc13bc6107762cb173c0380a752ba20f702c
viri <not.viri@protonmail.ch>
2019-03-04 01:30:26 -0800

    assets: redesign Script

---
+++ src/assets/script.rs
@@ -0,0 +1,44 @@
+#![allow(dead_code)] // Shut up.
+
+use crate::bytes::{ReadBytes, ReadString, WriteBytes, WriteString};
+use crate::types::Version;
+use std::io;
+
+pub const VERSION: Version = 800;
+
+pub struct Script {
+    /// The asset name present in GML and the editor.
+    pub name: String,
+
+    /// The full source code for the script.
+    pub source: String,
+}
+
+impl Script {
+    pub fn serialize<W>(&self, writer: &mut W) -> io::Result<usize>
+    where
+        W: io::Write,
+    {
+        let result = writer.write_pas_string(&self.name)?;
+        result += writer.write_u32_le(VERSION as u32)?;
+        result += writer.write_pas_string(&self.source)?;
+
+        Ok(result)
+    }
+
+    pub fn deserialize<B>(bin: B, strict: bool) -> io::Result<Script>
+    where
+        B: AsRef<[u8]>,
+    {
+        let mut reader = io::Cursor::new(bin.as_ref());
+        let name = reader.read_pas_string()?;
+        let version = reader.read_u32_le()?;
+        assert_eq!(version, VERSION);
+        let source = reader.read_pas_string()?;
+
+        Ok(Script {
+            name,
+            source,
+        })
+    }
+}
