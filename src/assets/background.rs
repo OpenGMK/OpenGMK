a2ca32d8ba5e63a73ddd8cf5fd25073e53a000ee
viri <not.viri@protonmail.ch>
2019-03-04 01:44:07 -0800

    assets: redesign Background, fix Script

---
+++ src/assets/background.rs
@@ -0,0 +1,83 @@
+#![allow(dead_code)] // Shut up.
+
+use crate::bytes::{ReadBytes, ReadString, WriteBytes, WriteString};
+use crate::types::{Dimensions, Version};
+use crate::util::bgra2rgba;
+use std::io::{self, Seek, SeekFrom};
+
+pub const VERSION1: Version = 710;
+pub const VERSION2: Version = 800;
+
+pub struct Background {
+    pub name: String,
+    pub size: Dimensions,
+    pub data: Option<Box<[u8]>>,
+}
+
+impl Background {
+    pub fn serialize<W>(&self, writer: &mut W) -> io::Result<usize>
+    where
+        W: io::Write,
+    {
+        let mut result = writer.write_pas_string(&self.name)?;
+        result += writer.write_u32_le(VERSION1 as u32)?;
+        result += writer.write_u32_le(VERSION2 as u32)?;
+        result += writer.write_u32_le(self.size.width as u32)?;
+        result += writer.write_u32_le(self.size.height as u32)?;
+        if let Some(data) = &self.data {
+            result += writer.write(&data)?;
+        }
+
+        Ok(result)
+    }
+
+    pub fn deserialize<B>(bin: B, strict: bool) -> io::Result<Background>
+    where
+        B: AsRef<[u8]>,
+    {
+        let mut reader = io::Cursor::new(bin.as_ref());
+        let name = reader.read_pas_string()?;
+
+        if strict {
+            let version1 = reader.read_u32_le()?;
+            let version2 = reader.read_u32_le()?;
+            assert_eq!(version1, VERSION1);
+            assert_eq!(version2, VERSION2);
+        } else {
+            reader.seek(SeekFrom::Current(8))?;
+        }
+
+        let width = reader.read_u32_le()?;
+        let height = reader.read_u32_le()?;
+        if width > 0 && height > 0 {
+            let data_len = reader.read_u32_le()?;
+
+            // sanity check
+            if data_len != (width * height * 4) {
+                panic!("Inconsistent pixel data length with dimensions");
+            }
+
+            // BGRA -> RGBA
+            let pos = reader.position() as usize;
+            let len = data_len as usize;
+            reader.seek(SeekFrom::Current(len as i64))?;
+            let mut buf = reader.get_ref()[pos..pos + len].to_vec();
+            bgra2rgba(&mut buf);
+
+            Ok(Background {
+                name,
+                size: Dimensions { width, height },
+                data: Some(buf.into_boxed_slice()),
+            })
+        } else {
+            Ok(Background {
+                name,
+                size: Dimensions {
+                    width: 0,
+                    height: 0,
+                },
+                data: None,
+            })
+        }
+    }
+}
--- src/assets/script.rs
+++ src/assets/script.rs
@@ -2,7 +2,7 @@
 
 use crate::bytes::{ReadBytes, ReadString, WriteBytes, WriteString};
 use crate::types::Version;
-use std::io;
+use std::io::{self, Seek, SeekFrom};
 
 pub const VERSION: Version = 800;
 
@@ -19,7 +19,7 @@ impl Script {
     where
         W: io::Write,
     {
-        let result = writer.write_pas_string(&self.name)?;
+        let mut result = writer.write_pas_string(&self.name)?;
         result += writer.write_u32_le(VERSION as u32)?;
         result += writer.write_pas_string(&self.source)?;
 
@@ -32,8 +32,14 @@ impl Script {
     {
         let mut reader = io::Cursor::new(bin.as_ref());
         let name = reader.read_pas_string()?;
-        let version = reader.read_u32_le()?;
-        assert_eq!(version, VERSION);
+        
+        if strict {
+            let version = reader.read_u32_le()?;
+            assert_eq!(version, VERSION);
+        } else {
+            reader.seek(SeekFrom::Current(4))?;
+        }
+
         let source = reader.read_pas_string()?;
 
         Ok(Script {
