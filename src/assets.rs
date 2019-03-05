2c21da784da81232e151f0453c79e5401877e116
viri <not.viri@protonmail.ch>
2019-03-05 10:45:33 -0600

    assets: redesign Font

--- src/assets.rs
+++ src/assets.rs
@@ -1,28 +1,15 @@
 pub mod background;
+pub mod font;
 pub mod path;
 pub mod script;
 pub mod sound;
 pub mod sprite;
 
 pub use self::background::Background;
+pub use self::font::Font;
 pub use self::path::Path;
 pub use self::script::Script;
 pub use self::sound::Sound;
 pub use self::sprite::Sprite;
 
-use crate::types::{Dimensions, Point};
-
 pub type GMCodeAction = u32;
-
-pub struct GMFont {
-    pub name: String,
-    pub sys_name: String,
-    pub size: u32,
-    pub bold: bool,
-    pub italic: bool,
-    pub range_start: u32,
-    pub range_end: u32,
-    pub dmap: Box<[u32; 0x600]>,
-    pub image_size: Dimensions,
-    pub image_data: Box<[u8]>,
-}
