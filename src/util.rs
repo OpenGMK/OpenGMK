29ffbae03638162793c56246534a5c8930a7e706
viri <not.viri@protonmail.ch>
2019-03-12 16:42:03 -0600

    game (parser): parallelize all asset chunks

--- src/util.rs
+++ src/util.rs
@@ -21,7 +21,7 @@ mod tests {
         let bgra_pixels = [0u8, 1, 54, 242, 192, 24, 65, 6, 35, 98, 4, 20];
         let rgba_pixels = [54u8, 1, 0, 242, 65, 24, 192, 6, 4, 98, 35, 20];
         let mut cool_pixels = bgra_pixels.to_vec();
-        
+
         bgra2rgba(&mut cool_pixels);
         assert_eq!(cool_pixels, rgba_pixels);
         rgba2bgra(&mut cool_pixels);
