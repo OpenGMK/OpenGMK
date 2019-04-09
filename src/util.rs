a4e2272aef9d16dc338492c7e3b46da3facf1ba6
viri <not.viri@protonmail.ch>
2019-04-09 15:39:45 -0600

    util: optimize bgra2rgba (thank you, Broken Pen!)

--- src/util.rs
+++ src/util.rs
@@ -1,15 +1,9 @@
-// Converts BGRA pixeldata to RGBA pixeldata in-place.
+/// Converts BGRA pixeldata to RGBA pixeldata in-place.
 pub fn bgra2rgba(data: &mut [u8]) {
-    let len = data.len();
-    let mut tmp: u8; // blue <> red buffer
-    for i in (0..len).step_by(4) {
-        tmp = data[i]; // buf = blue
-        data[i] = data[i + 2]; // blue = red
-        data[i + 2] = tmp; // red = buf
-    }
+    data.chunks_exact_mut(4).for_each(|chunk| chunk.swap(0, 2));
 }
 
-// Converts RGBA pixeldata to BGRA pixeldata in-place.
+/// Converts RGBA pixeldata to BGRA pixeldata in-place.
 pub use bgra2rgba as rgba2bgra;
 
 #[cfg(test)]
