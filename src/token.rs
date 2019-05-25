b5d6feba9b7a08367e8c25d67e5c4513039c1e6d
viri <not.viri@protonmail.ch>
2019-05-25 11:46:28 -0600

    gml (token): populate Operator enum

--- src/token.rs
+++ src/token.rs
@@ -33,7 +33,98 @@ pub enum Keyword {
 }
 
 #[derive(Debug)]
-pub enum Operator {}
+pub enum Operator {
+    /// `+` Add or unary positive (redundant)
+    Add,
+    
+    /// `-` Subtract or unary negative
+    Subtract,
+
+    /// `*` Multiply
+    Multiply,
+
+    /// `/` Divide
+    Divide,
+
+    /// `div` Integer Divide (Divide, Floor)
+    IntDivide,
+
+    /// `&` Binary AND
+    BinaryAnd,
+
+    /// `|` Binary OR
+    BinaryOr,
+
+    /// `^` Binary XOR
+    BinaryXor,
+
+    /// `=` Assign
+    /// NOTE: This operator means Equal (`==`) if read in an expression
+    Assign,
+
+    /// `!` Boolean NOT
+    Not,
+
+    /// `<` Less Than (RHS)
+    LessThan,
+
+    /// `>` Greater Than (RHS)
+    GreaterThan,
+
+    /// `+=` Assignment Add
+    AssignAdd,
+
+    /// `-=` Assignment Subtract
+    AssignSubtract,
+
+    /// `*=` Assignment Multiply
+    AssignMultiply,
+
+    /// `/=` Assignment Divide
+    AssignDivide,
+
+    /// `&=` Assignment Binary AND
+    AssignBinaryAnd,
+
+    /// `|=` Assignment Binary OR
+    AssignBinaryOr,
+
+    /// `^=` Assignment Binary XOR
+    AssignBinaryXor,
+
+    /// `==` Equal
+    Equal,
+
+    /// `!=` Not Equal
+    NotEqual,
+
+    /// `<=` Less Than or Equal
+    LessThanOrEqual,
+
+    /// `>=` Greater Than or Equal
+    GreaterThanOrEqual,
+
+    /// `mod` Modulo
+    Modulo,
+
+    /// `&&` Boolean AND
+    And,
+
+    /// `||` Boolean OR
+    Or,
+
+    /// `^^` Boolean XOR
+    Xor,
+
+    /// `<<` Binary Shift Left
+    BinaryShiftLeft,
+
+    /// `>>` Binary Shift Right
+    BinaryShiftRight,
+
+    /// `~` Binary Complement (Unary)
+    Complement,
+}
 
 #[derive(Debug)]
 pub enum Separator {}
