5871952b9f429e7839dabbe8359bb4af52303e80
viri <not.viri@protonmail.ch>
2019-05-24 23:51:17 -0600

    gml (lexer): support basic identifiers

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,4 @@
-use crate::token::Token;
+use crate::token::*;
 
 use std::iter::{Enumerate, Peekable};
 use std::str::Bytes;
@@ -31,7 +31,58 @@ impl<'a> Iterator for Lexer<'a> {
 
     fn next(&mut self) -> Option<Token<'a>> {
         self.fast_forward(); // locate next token
+       
+        // this is fine since we operate on something that is a &str in a first place
+        // we should of course never use a value not pulled from peek() as range indices
+        let src = self.src; // since &mut self
+        macro_rules! to_str {
+            ($src: expr, $range: expr) => ({
+                unsafe {
+                    std::str::from_utf8_unchecked(
+                        $src.as_bytes()
+                            .get_unchecked($range)
+                    )
+                }
+            })
+        };
+        
+        let head = *self.iter.peek()?;
+        println!("we at '{}'", &self.src[(head.0)..]);
+        match head.1 {
+            b'A'...b'Z' | b'a'... b'z' | b'_' => {
+                let identifier = {
+                    let mut last = head;
+                    loop {
+                        match self.iter.next() {
+                            Some(tail) => match tail.1 {
+                                b'A'...b'Z' | b'a'...b'z' | b'0'...b'9' | b'_' => last = tail,
+                                _ => break to_str!(src, head.0..tail.0),
+                            },
+                            None => break to_str!(src, head.0..=last.0),
+                        }
+                    }
+                };
+                return Some(Token::Identifier(identifier));
+            },
 
+            b'0' ..= b'9' | b'.' => {
+                // inhale real
+            },
+
+            b'"' | b'\'' => {
+                // inhale string
+            },
+
+            b'$' => {
+                // inhale hex literal
+            },
+
+            0x00 ..= b'~' => {
+                // operator possibly
+            },
+
+            _ => panic!("Oh no! Corrupt character: {} ({:#X})", head.1, head.1),
+        }
         None
     }
 }
