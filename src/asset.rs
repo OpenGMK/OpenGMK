9b5105010ec695a725eeea26774042d736f965c3
viri <git@stuff.moe>
2019-08-29 19:07:39 -0600

    asset: implement Asset for Script

--- src/asset.rs
+++ src/asset.rs
@@ -10,33 +10,38 @@ use std::{
 };
 
 pub trait Asset {
-    fn deserialize<B>(from: B, strict: bool, version: GameVersion) -> Result<Self, AssetDataError> where B: AsRef<[u8]>, Self: Sized;
-    fn serialize<W>(&self, to: &mut W) -> io::Result<usize> where W: io::Write;
+    fn deserialize<B>(from: B, strict: bool, version: GameVersion) -> Result<Self, AssetDataError>
+    where
+        B: AsRef<[u8]>,
+        Self: Sized;
+    fn serialize<W>(&self, to: &mut W) -> io::Result<usize>
+    where
+        W: io::Write;
 }
 
 #[derive(Debug)]
 pub enum AssetDataError {
     IO(io::Error),
-    VersionError {
-        expected: u32,
-        got: u32
-    }
+    VersionError { expected: u32, got: u32 },
 }
 
 impl Error for AssetDataError {}
 impl Display for AssetDataError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", match self {
-            AssetDataError::IO(err) => format!("io error: {}", err),
-            AssetDataError::VersionError { expected, got }
-                => format!(
+        write!(
+            f,
+            "{}",
+            match self {
+                AssetDataError::IO(err) => format!("io error: {}", err),
+                AssetDataError::VersionError { expected, got } => format!(
                     "version error: expected {} ({}), found {} ({})",
                     *expected,
                     *expected as f32 / 100.0,
                     *got,
                     *got as f32 / 100.0
-                )
-        })
+                ),
+            }
+        )
     }
 }
 
@@ -52,5 +57,14 @@ impl From<(u32, u32)> for AssetDataError {
             expected: version_error.0,
             got: version_error.1,
         }
+    }
+}
+
+#[inline(always)]
+fn assert_ver(got: u32, expected: u32) -> Result<(), AssetDataError> {
+    if got != expected {
+        Err(AssetDataError::VersionError { expected, got })
+    } else {
+        Ok(())
     }
 }
