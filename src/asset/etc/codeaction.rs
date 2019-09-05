d582b55b14f555010bf4d029d613367092dbcecf
viri <git@stuff.moe>
2019-09-04 18:30:37 -0600

    assets/etc: implement CodeAction (DnD types)

---
+++ src/asset/etc/codeaction.rs
@@ -0,0 +1,155 @@
+use crate::asset::{assert_ver, AssetDataError};
+use crate::byteio::{ReadBytes, ReadString, WriteBytes, WriteString};
+use crate::def::ID;
+use std::io::{self, Cursor, Seek, SeekFrom};
+
+pub const VERSION: u32 = 440;
+pub const PARAM_COUNT: usize = 8;
+
+pub struct CodeAction {
+    /// Unique ID that identifies what type of DnD action this is.
+    pub id: u32,
+
+    /// Instance this applies to.
+    pub applies_to: ID,
+
+    /// Indicates whether this DnD is a condition,
+    /// ie. whether the following DnD should be dependent on the evaluation of this one.
+    pub is_condition: bool,
+
+    /// Whether the "NOT" checkbox is checked.
+    /// All DnDs have this property, even ones which don't have a NOT option.
+    pub invert_condition: bool,
+
+    /// Whether the "relative" checkbox is checked.
+    /// All DnDs have this property, even ones which don't have a "relative" option.
+    pub is_relative: bool,
+
+    /// What action library the action is loaded from (GameMaker 8 Runner).
+    pub lib_id: u32,
+
+    /// What tab it appears in inside the GameMaker 8 IDE.
+    pub action_kind: u32,
+
+    /// What offset appears in the GameMaker 8 IDE in its respective tab.
+    pub action_idx: u32,
+
+    /// Whether the relative checkbox appears in the GameMaker 8 IDE.
+    pub can_be_relative: u32,
+
+    /// Whether you can change execution target in the GameMaker 8 IDE.
+    pub applies_to_something: bool,
+
+    /// [TODO] Name of the function if provided.
+    pub fn_name: String,
+
+    /// [TODO] Probably the code of the custom Drag-and-Drop.
+    pub fn_code: String,
+
+    pub param_count: usize,
+    pub param_types: [u32; PARAM_COUNT],
+    pub param_strings: [String; PARAM_COUNT],
+}
+
+impl CodeAction {
+    pub fn from_cur<B>(reader: &mut Cursor<B>, strict: bool) -> Result<Self, AssetDataError>
+    where
+        B: AsRef<[u8]>,
+    {
+        if strict {
+            let version = reader.read_u32_le()?;
+            assert_ver(version, VERSION)?;
+        } else {
+            reader.seek(SeekFrom::Current(4))?;
+        }
+
+        let lib_id = reader.read_u32_le()?;
+        let id = reader.read_u32_le()?;
+        let action_kind = reader.read_u32_le()?;
+        let can_be_relative = reader.read_u32_le()?;
+        let is_condition = reader.read_u32_le()? != 0;
+        let applies_to_something = reader.read_u32_le()? != 0;
+        let action_idx = reader.read_u32_le()?;
+
+        let fn_name = reader.read_pas_string()?;
+        let fn_code = reader.read_pas_string()?;
+
+        let param_count = reader.read_u32_le()? as usize;
+        if param_count > PARAM_COUNT {
+            return Err(AssetDataError::MalformedData);
+        }
+
+        // type count - should always be 8
+        if reader.read_u32_le()? as usize != PARAM_COUNT {
+            return Err(AssetDataError::MalformedData);
+        }
+
+        let mut param_types = [0u32; PARAM_COUNT];
+        for i in 0..PARAM_COUNT {
+            param_types[i] = reader.read_u32_le()?;
+        }
+
+        let applies_to = reader.read_i32_le()? as ID;
+        let is_relative = reader.read_u32_le()? != 0;
+
+        // arg count - should always be 8
+        if reader.read_u32_le()? as usize != PARAM_COUNT {
+            return Err(AssetDataError::MalformedData);
+        }
+
+        let mut param_strings: [String; 8] = Default::default();
+        for i in 0..PARAM_COUNT {
+            param_strings[i] = reader.read_pas_string()?;
+        }
+
+        let invert_condition = reader.read_u32_le()? != 0;
+
+        Ok(CodeAction {
+            id,
+            applies_to,
+            is_condition,
+            invert_condition,
+            is_relative,
+            lib_id,
+            action_kind,
+            can_be_relative,
+            applies_to_something,
+            action_idx,
+            fn_name,
+            fn_code,
+            param_count,
+            param_types,
+            param_strings,
+        })
+    }
+
+    pub fn write_to<W>(&self, writer: &mut W) -> io::Result<usize>
+    where
+        W: io::Write,
+    {
+        let mut result = writer.write_u32_le(VERSION)?;
+        result += writer.write_u32_le(self.lib_id)?;
+        result += writer.write_u32_le(self.id)?;
+        result += writer.write_u32_le(self.action_kind)?;
+        result += writer.write_u32_le(self.can_be_relative)?;
+        result += writer.write_u32_le(self.is_condition as u32)?;
+        result += writer.write_u32_le(self.applies_to_something as u32)?;
+        result += writer.write_u32_le(self.action_idx)?;
+        result += writer.write_pas_string(&self.fn_name)?;
+        result += writer.write_pas_string(&self.fn_code)?;
+        result += writer.write_u32_le(self.param_count as u32)?;
+        result += writer.write_u32_le(PARAM_COUNT as u32)?;
+        for i in &self.param_types {
+            result += writer.write_u32_le(*i)?;
+        }
+        result += writer.write_i32_le(self.applies_to)?;
+        result += writer.write_u32_le(self.is_relative as u32)?;
+        result += writer.write_u32_le(PARAM_COUNT as u32)?;
+        for i in &self.param_strings {
+            result += writer.write_pas_string(i)?;
+        }
+        result += writer.write_u32_le(self.invert_condition as u32)?;
+
+        Ok(result)
+    }
+}
