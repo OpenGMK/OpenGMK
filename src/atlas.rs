260fff83e6947366cec8e62e0b42ae2eb8f5d5eb
viri <git@stuff.moe>
2019-10-07 21:13:16 -0600

    atlas::AtlasBuilder: track max x/y values per packer

--- src/atlas.rs
+++ src/atlas.rs
@@ -1,8 +1,9 @@
 use rect_packer::DensePacker;
+use std::cmp::max;
 
 pub struct AtlasBuilder {
     max_size: i32,
-    packers: Vec<DensePacker>,
+    packers: Vec<(DensePacker, i32, i32)>, // max_x, max_y
 }
 
 pub struct AtlasRef {
@@ -23,8 +24,10 @@ impl AtlasBuilder {
     }
 
     pub fn add(&mut self, w: i32, h: i32) -> AtlasRef {
-        for (i, pk) in self.packers.iter_mut().enumerate() {
+        for (i, (pk, max_x, max_y)) in self.packers.iter_mut().enumerate() {
             if let Some(rect) = pk.pack(w as _, h as _, false) {
+                *max_x = max(*max_x, rect.x + rect.width);
+                *max_y = max(*max_y, rect.y + rect.height);
                 return AtlasRef {
                     atlas_id: i as _,
                     w: rect.width,
@@ -38,7 +41,7 @@ impl AtlasBuilder {
         if w > self.max_size || h > self.max_size {
             panic!("what the fuck that's big"); // TODO lol
         } else {
-            self.packers.push(DensePacker::new(self.max_size, self.max_size));
+            self.packers.push((DensePacker::new(self.max_size, self.max_size), 0, 0));
             self.add(w, h)
         }
     }
