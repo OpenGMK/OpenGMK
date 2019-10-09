7e8d2a8b559a39828d8ddc8e974bf514c2d5d091
GM8Emulator <gm8emulator@gmail.com>
2019-10-09 01:27:23 +0100

    asset::extension: fix reading .lib files

--- lib/gm8x/src/asset/extension.rs
+++ lib/gm8x/src/asset/extension.rs
@@ -209,57 +209,62 @@ impl Extension {
         let data_pos = reader.position() as usize;
         reader.seek(SeekFrom::Current(contents_len as _))?;
 
-        // decryption setup
-        let mut char_table = [0u8; 0x200];
-        let mut seed1: i32 = seed1_raw as _;
-        let mut seed2: i32 = (seed1 % 0xFA) + 6;
-        seed1 /= 0xFA;
-        if seed1 < 0 {
-            seed1 += 100;
-        }
-        if seed2 < 0 {
-            seed2 += 100;
-        }
-        for i in 0..char_table.len() {
-            char_table[i] = (i % 256) as u8; // 0-255 then 0-255 again
-        }
+        // Don't do decryption if there are no contents
+        if contents_len != 0 {
+            // decryption setup
+            let mut char_table = [0u8; 0x200];
+            let mut seed1: i32 = seed1_raw as _;
+            let mut seed2: i32 = (seed1 % 0xFA) + 6;
+            seed1 /= 0xFA;
+            if seed1 < 0 {
+                seed1 += 100;
+            }
+            if seed2 < 0 {
+                seed2 += 100;
+            }
+            for i in 0..char_table.len() {
+                char_table[i] = (i % 256) as u8; // 0-255 then 0-255 again
+            }
 
-        // calculating char table - pass 1: pseudorandom byteswap
-        for i in 1..0x2711 {
-            let idx: usize = ((((i * seed2 as u32) + seed1 as u32) % 0xFE) + 1) as _;
-            let b1 = char_table[idx];
-            let b2 = char_table[idx + 1];
-            char_table[idx] = b2;
-            char_table[idx + 1] = b1;
+            // calculating char table - pass 1: pseudorandom byteswap
+            for i in 1..0x2711 {
+                let idx: usize = ((((i * seed2 as u32) + seed1 as u32) % 0xFE) + 1) as _;
+                let b1 = char_table[idx];
+                let b2 = char_table[idx + 1];
+                char_table[idx] = b2;
+                char_table[idx + 1] = b1;
+            }
+
+            // .. pass 2: use low half to scramble top half
+            for i in 0..0x100 {
+                let lo: u8 = char_table[i + 1];
+                char_table[lo as usize + 0x100] = (i as u8).wrapping_add(1);
+            }
+
+            // decrypt data chunk
+            for byte in &mut reader.get_mut()[data_pos + 1..data_pos + contents_len] {
+                *byte = char_table[*byte as usize + 0x100];
+            }
+
+            let end_pos = reader.position() as usize;
+            reader.set_position(data_pos as u64);
+
+            // write file chunks
+            for file in &mut files {
+                if file.kind != FileKind::ActionLibrary {
+                    let len = reader.read_u32_le()? as usize;
+                    let pos = reader.position() as usize;
+
+                    reader.seek(SeekFrom::Current(len as i64))?; // pre-check for next get
+                    file.contents = match inflate(reader.get_ref().get(pos..pos + len).unwrap_or_else(|| unreachable!())) {
+                        Ok(x) => x.into_boxed_slice(),
+                        Err(_) => return Err(AssetDataError::MalformedData),
+                    };
+                }
+            }
+
+            reader.set_position(end_pos as u64);
         }
-
-        // .. pass 2: use low half to scramble top half
-        for i in 0..0x100 {
-            let lo: u8 = char_table[i + 1];
-            char_table[lo as usize + 0x100] = (i as u8).wrapping_add(1);
-        }
-
-        // decrypt data chunk
-        for byte in &mut reader.get_mut()[data_pos + 1..data_pos + contents_len] {
-            *byte = char_table[*byte as usize + 0x100];
-        }
-
-        let end_pos = reader.position() as usize;
-        reader.set_position(data_pos as u64);
-
-        // write file chunks
-        for file in &mut files {
-            let len = reader.read_u32_le()? as usize;
-            let pos = reader.position() as usize;
-
-            reader.seek(SeekFrom::Current(len as i64))?; // pre-check for next get
-            file.contents = match inflate(reader.get_ref().get(pos..pos + len).unwrap_or_else(|| unreachable!())) {
-                Ok(x) => x.into_boxed_slice(),
-                Err(_) => return Err(AssetDataError::MalformedData),
-            };
-        }
-
-        reader.set_position(end_pos as u64);
 
         Ok(Extension {
             name,
