3a0a295e2d599aa08e102b436a2eea44ce06ec38
viri <git@stuff.moe>
2019-11-28 01:00:17 -0600

    gml::rand: implement next_int, add inline on fns

--- src/gml/rand.rs
+++ src/gml/rand.rs
@@ -15,16 +15,27 @@ pub const MULTIPLIER: i32 = 0x8088405;
 pub const INT_STEP: u64 = 0x3DF0000000000000;
 
 impl Random {
-    pub fn new(seed: i32) -> Self {
+    #[inline]
+    pub const fn new(seed: i32) -> Self {
         Self(seed)
     }
 
+    #[inline]
     pub fn cycle(&mut self) {
         self.0 = self.0.wrapping_mul(MULTIPLIER).wrapping_add(INCREMENT);
     }
 
+    #[inline]
     pub fn next(&mut self, bound: f64) -> f64 {
         self.cycle(); // cycle seed
         (self.0 as u32 as f64) * f64::from_bits(INT_STEP) * bound
+    }
+
+    #[inline]
+    pub fn next_int(&mut self, bound: i32) -> i32 {
+        self.cycle(); // cycle seed
+        let ls = (self.0 as u64) & 0xFFFFFFFF;
+        let lb = i64::from(bound).wrapping_add(1);
+        ((ls.wrapping_mul(lb as u64)) >> 32) as i32
     }
 }
