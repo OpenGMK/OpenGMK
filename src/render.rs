9670f374d91a7d459e4a987e76fd0498d759539a
viri <git@stuff.moe>
2019-12-17 17:51:15 -0600

    render: add process_atlases Renderer fn
    + partially implement skeleton for OpenGLRenderer
    + comment out shit in game.rs so it actually builds (for CI mainly)

--- src/render.rs
+++ src/render.rs
@@ -2,5 +2,13 @@
 
 pub mod opengl;
 
-pub trait Renderer {}
+use crate::atlas::AtlasBuilder;
+
+pub trait Renderer {
+    /// Stores & uploads atlases to the GPU.
+    /// This function is for initializing, and should be called only once.
+    ///
+    /// Returns a handle to each inserted texture (in insertion order).
+    fn process_atlases(&mut self, atl: AtlasBuilder) -> Vec<Texture>;
+}
 pub struct Texture(usize);
