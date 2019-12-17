69f05d107858db6f2e405182d5791d4616486d01
viri <git@stuff.moe>
2019-12-17 17:19:25 -0600

    atlas: redesign AtlasBuilder

--- src/atlas.rs
+++ src/atlas.rs
@@ -1,9 +1,14 @@
 use rect_packer::DensePacker;
-use std::cmp::max;
+
+#[inline]
+fn next_pow2(n: i32) -> i32 {
+    2i32.pow((n as f32).log2().ceil() as _)
+}
 
 pub struct AtlasBuilder {
     max_size: i32,
-    packers: Vec<(DensePacker, i32, i32)>, // max_x, max_y
+    packers: Vec<DensePacker>,
+    textures: Vec<(AtlasRef, Box<[u8]>)>,
 }
 
 #[derive(Clone)]
@@ -17,43 +22,65 @@ pub struct AtlasRef {
 }
 
 impl AtlasBuilder {
-    pub fn new(max_wh: i32) -> Self {
+    pub fn new(max_size: i32) -> Self {
+        assert_eq!(max_size, next_pow2(max_size));
         AtlasBuilder {
-            max_size: max_wh,
-            packers: Vec::with_capacity(1),
+            max_size,
+            packers: Vec::new(),
+            textures: Vec::new(),
         }
     }
 
-    pub fn add(&mut self, w: i32, h: i32) -> AtlasRef {
-        for (i, (pk, max_x, max_y)) in self.packers.iter_mut().enumerate() {
-            if let Some(rect) = pk.pack(w as _, h as _, false) {
-                *max_x = max(*max_x, rect.x + rect.width);
-                *max_y = max(*max_y, rect.y + rect.height);
-                return AtlasRef {
-                    atlas_id: i as _,
+    pub fn texture(&mut self, width: i32, height: i32, data: Box<[u8]>) -> bool {
+        fn to_texture(id: u32, rect: rect_packer::Rect, data: Box<[u8]>) -> (AtlasRef, Box<[u8]>) {
+            (
+                AtlasRef {
+                    atlas_id: id,
                     w: rect.width,
                     h: rect.height,
                     x: rect.x,
                     y: rect.y,
-                };
+                },
+                data,
+            )
+        }
+
+        if width > self.max_size || height > self.max_size {
+            return false;
+        }
+
+        for (id, packer) in self.packers.iter_mut().enumerate() {
+            if let Some(rect) = packer.pack(width, height, false) {
+                self.textures.push(to_texture(id as _, rect, data));
+                return true;
+            } else {
+                let mut clone = packer.clone();
+                loop {
+                    let (pwidth, pheight) = packer.size();
+                    if pwidth <= pheight && (pwidth * 2) < self.max_size {
+                        clone.resize(pwidth * 2, pheight);
+                    } else if (pheight * 2) < self.max_size {
+                        clone.resize(pwidth, pheight * 2);
+                    } else {
+                        break;
+                    }
+
+                    if let Some(rect) = packer.pack(width, height, false) {
+                        *packer = clone;
+                        self.textures.push(to_texture(id as _, rect, data));
+                        return true;
+                    }
+                }
             }
         }
 
-        assert!(w <= self.max_size);
-        assert!(h <= self.max_size);
-        self.packers
-            .push((DensePacker::new(self.max_size, self.max_size), 0, 0));
-        self.add(w, h)
+        let size = 4096.min(self.max_size);
+        self.packers.push(DensePacker::new(size, size));
+        self.texture(width, height, data)
     }
 
-    /// Resizes all atlases to nearest powers of 2.
-    pub fn harden(&mut self) {
-        fn next_pow2(n: f32) -> i32 {
-            2_i32.pow(n.log2().ceil() as _)
-        }
-
-        for (packer, w, h) in &mut self.packers {
-            packer.resize(next_pow2(*w as _) as _, next_pow2(*h as _) as _);
-        }
+    #[allow(clippy::type_complexity)] // It's for the Renderer only.
+    pub fn into_inner(self) -> (Vec<DensePacker>, Vec<(AtlasRef, Box<[u8]>)>) {
+        (self.packers, self.textures)
     }
 }
