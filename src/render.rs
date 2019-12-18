960ebd09b2f5ed4e8b0ab59369c40344cf912230
viri <git@stuff.moe>
2019-12-18 14:32:04 -0600

    render::opengl: finish upload_atlases, impl drop

--- src/render.rs
+++ src/render.rs
@@ -9,7 +9,7 @@ pub trait Renderer {
     /// This function is for initializing, and should be called only once.
     ///
     /// Returns a handle to each inserted texture (in insertion order).
-    fn upload_atlases(&mut self, atl: AtlasBuilder) -> Result<Vec<Texture>, String>;
+    fn upload_atlases(&mut self, atl: AtlasBuilder) -> Result<(), String>;
 
     /// Returns the max texture size the GPU can hold.
     fn max_gpu_texture_size(&self) -> usize;
--- src/render/opengl.rs
+++ src/render/opengl.rs
@@ -10,7 +10,7 @@ mod gl {
 
 use crate::{
     atlas::{AtlasBuilder, AtlasRef},
-    render::{Renderer, Texture},
+    render::Renderer,
 };
 use glutin::{
     event_loop::EventLoop,
@@ -18,7 +18,7 @@ use glutin::{
     ContextWrapper, PossiblyCurrent, {Api, ContextBuilder, GlProfile, GlRequest},
 };
 use rect_packer::DensePacker;
-use std::ptr;
+use std::{ops::Drop, ptr};
 
 // OpenGL typedefs
 use gl::types::{GLint, GLuint};
@@ -28,10 +28,15 @@ pub struct OpenGLRenderer {
     el: EventLoop<()>,
     window: Window,
 
-    // texture atlases
+    // -- TEXTURE ATLASES --
+    /// Whether the initial atlases have been uploaded (see upload_atlases).
     atlases_initialized: bool,
+    /// Atlases' rectangle packers to be reused for dynamic sprite loading.
     atlas_packers: Vec<DensePacker>,
-    atlas_refs: Vec<AtlasRef>, // `Texture` indexes this
+    /// Atlas references (xywh + idx) to be indexed by `Texture`s.
+    atlas_refs: Vec<AtlasRef>,
+    /// OpenGL's texture handles in identical order to the atlases.
+    texture_ids: Vec<GLuint>,
 }
 
 pub struct OpenGLRendererOptions<'a> {
@@ -83,6 +88,7 @@ impl OpenGLRenderer {
             atlases_initialized: false,
             atlas_packers: Vec::new(),
             atlas_refs: Vec::new(),
+            texture_ids: Vec::new(),
         })
     }
 }
@@ -96,7 +102,7 @@ impl Renderer for OpenGLRenderer {
         }
     }
 
-    fn upload_atlases(&mut self, atl: AtlasBuilder) -> Result<Vec<Texture>, String> {
+    fn upload_atlases(&mut self, atl: AtlasBuilder) -> Result<(), String> {
         assert!(!self.atlases_initialized, "atlases should be initialized only once");
 
         let (packers, sprites) = atl.into_inner();
@@ -126,7 +132,7 @@ impl Renderer for OpenGLRenderer {
 
             // upload textures
             let mut current_texture: GLint = 0;
-            for (atl_ref, pixels) in sprites {
+            for (atl_ref, pixels) in &sprites {
                 if current_texture != atl_ref.atlas_id as _ {
                     gl::BindTexture(gl::TEXTURE_2D, textures[atl_ref.atlas_id as usize]);
                     current_texture = atl_ref.atlas_id as _;
@@ -145,27 +151,29 @@ impl Renderer for OpenGLRenderer {
                 );
             }
 
-            // DELETE THIS AFTER ETESTING OK ?? ? ? OK !
-            for ((i, texture), packer) in textures.iter().enumerate().zip(packers.iter()) {
-                gl::BindTexture(gl::TEXTURE_2D, *texture);
-                let w = std::io::BufWriter::new(std::fs::File::create(format!("./atlas{}.png", i)).unwrap());
-                let (width, height) = packer.size();
-                let mut encoder = png::Encoder::new(w, width as _, height as _);
-                encoder.set_color(png::ColorType::RGBA);
-                encoder.set_depth(png::BitDepth::Eight);
-                let mut writer = encoder.write_header().unwrap();
+            // -- ATLAS DUMPER, DEBUGGING ONLY! UNCOMMENT --
+            // for ((i, texture), packer) in textures.iter().enumerate().zip(packers.iter()) {
+            //     gl::BindTexture(gl::TEXTURE_2D, *texture);
+            //     let w = std::io::BufWriter::new(
+            //         std::fs::File::create(format!("./atlas{}.png", i)).unwrap()
+            //     );
+            //     let (width, height) = packer.size();
+            //     let mut encoder = png::Encoder::new(w, width as _, height as _);
+            //     encoder.set_color(png::ColorType::RGBA);
+            //     encoder.set_depth(png::BitDepth::Eight);
+            //     let mut writer = encoder.write_header().unwrap();
 
-                let mut buf = vec![0u8; width as usize * height as usize * 4];
-                gl::GetTexImage(
-                    gl::TEXTURE_2D,
-                    0,
-                    gl::RGBA,
-                    gl::UNSIGNED_BYTE,
-                    buf.as_mut_ptr() as *mut _,
-                );
+            //     let mut buf = vec![0u8; width as usize * height as usize * 4];
+            //     gl::GetTexImage(
+            //         gl::TEXTURE_2D,
+            //         0,
+            //         gl::RGBA,
+            //         gl::UNSIGNED_BYTE,
+            //         buf.as_mut_ptr() as *mut _,
+            //     );
 
-                writer.write_image_data(&buf).unwrap();
-            }
+            //     writer.write_image_data(&buf).unwrap();
+            // }
 
             // verify it actually worked
             match gl::GetError() {
@@ -173,12 +181,24 @@ impl Renderer for OpenGLRenderer {
                 err => return Err(format!("Failed to upload textures to GPU! (OpenGL code {})", err)),
             }
 
-            // delete the textures
-            gl::DeleteTextures(textures.len() as _, &textures as *const _ as *mut _);
+            // store opengl texture handles
+            self.texture_ids = textures;
         }
 
-        println!("LAST OPENGL ERROR: {}", unsafe { gl::GetError() });
+        // store packers, discard pixeldata
+        self.atlas_packers = packers;
+        self.atlas_refs = sprites.into_iter().map(|(x, _)| x).collect();
 
-        unimplemented!()
+        // generate texture handles
+        self.atlases_initialized = true;
+        Ok(())
+    }
+}
+
+impl Drop for OpenGLRenderer {
+    fn drop(&mut self) {
+        unsafe {
+            gl::DeleteTextures(self.texture_ids.len() as _, self.texture_ids.as_mut_ptr() as *mut _);
+        }
     }
 }
