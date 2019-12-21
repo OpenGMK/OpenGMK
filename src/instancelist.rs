4a0c5324cbb0b18b0af9a9bd18afb4f669aae45f
Adam <gm8emulator@gmail.com>
2019-12-21 22:26:16 +0000

    instancelist: initial commit

---
+++ src/instancelist.rs
@@ -0,0 +1,38 @@
+use crate::instance::Instance;
+use generational_arena::{Arena, Index};
+
+pub struct InstanceList {
+    pub arena: Arena<Instance>,
+}
+
+impl InstanceList {
+    pub fn new() -> Self {
+        Self {
+            arena: Arena::with_capacity(1024),
+        }
+    }
+
+    pub fn insert(&mut self, instance: Instance) -> Index {
+        self.arena.insert(instance)
+    }
+
+    pub fn remove(&mut self, index: Index) -> Option<Instance> {
+        self.arena.remove(index)
+    }
+
+    pub fn remove_deleted(&mut self) {
+        self.arena.retain(|_, x| x.exists)
+    }
+
+    pub fn remove_non_persistent(&mut self) {
+        self.arena.retain(|_, x| x.persistent)
+    }
+
+    pub fn get(&self, index: Index) -> Option<&Instance> {
+        self.arena.get(index)
+    }
+
+    pub fn get_mut(&mut self, index: Index) -> Option<&mut Instance> {
+        self.arena.get_mut(index)
+    }
+}
