cf1f9a574d23f6570a85ff04f7723819c01d80c8
GM8Emulator <gm8emulator@gmail.com>
2020-01-08 10:56:27 +0000

    glsl: remove camel case

--- src/render/glsl/vertex.glsl
+++ src/render/glsl/vertex.glsl
@@ -1,10 +1,10 @@
 #version 330 core
 
-layout (location = 0) in vec3 aPos;
+layout (location = 0) in vec3 pos;
 in mat4 model_view;
 
 uniform mat4 projection;
 
 void main() {
-    gl_Position = projection * model_view * vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    gl_Position = projection * model_view * vec4(pos.x, pos.y, pos.z, 1.0);
 }
