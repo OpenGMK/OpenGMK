f6efd07b4a6d9d4b37bbdfdf2d3a13a9703c9e00
GM8Emulator <gm8emulator@gmail.com>
2020-01-13 12:01:29 +0000

    compiler: add Error type, dummy compile_action fn

--- src/gml/compiler.rs
+++ src/gml/compiler.rs
@@ -9,7 +9,14 @@ pub struct Compiler {
     pub fields: Vec<String>,
 }
 
+pub enum Error {
+    ASTError(String),
+    GMLError(String),
+    DnDError(String),
+}
+
 impl Compiler {
+    /// Create a compiler. The size hint indicates how many constants are likely to be entered.
     pub fn new(constants_size_hint: usize) -> Self {
         let mut constants = HashMap::with_capacity(constants_size_hint + super::CONSTANTS.len());
         super::CONSTANTS.iter().for_each(|(name, value)| {
@@ -21,8 +28,14 @@ impl Compiler {
         }
     }
 
-    pub fn compile(source: &str) -> Result<Vec<Instruction>, String> {
-        let ast = ast::AST::new(source).map_err(|e| e.message)?;
+    /// Compile the basic components of a drag-and-drop action into a list of Instructions.
+    pub fn compile_action(&mut self) -> Result<Vec<Instruction>, Error> {
+        // TODO: this
+        Ok(vec![])
+    }
+
+    pub fn compile(&mut self, source: &str) -> Result<Vec<Instruction>, Error> {
+        let ast = ast::AST::new(source).map_err(|e| Error::ASTError(e.message))?;
 
         let instructions = Vec::new();
         for _node in ast.into_iter() {
