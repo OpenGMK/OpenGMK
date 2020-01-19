099cd01f91326eee25359e27385e325432de70a5
Adam <gm8emulator@gmail.com>
2020-01-19 02:46:49 +0000

    runtime: add more Node types

--- src/gml/runtime.rs
+++ src/gml/runtime.rs
@@ -1,3 +1,4 @@
+use std::rc::Rc;
 use super::Value;
 
 #[derive(Debug)]
@@ -11,13 +12,27 @@ pub enum Node {
     Literal {
         value: Value,
     },
-    Function, // TODO
+    Function {
+        args: Box<[Node]>,
+        function: fn(&[Value]) -> Value,
+    },
+    Script {
+        args: Box<[Node]>,
+        script: Rc<[Instruction]>,
+    },
+    Field, // TODO
     Variable, // TODO
+    GlobalVariable, // TODO
     Binary {
         left: Box<Node>,
         right: Box<Node>,
         operator: fn(&Value, &Value) -> Value,
     },
+    Unary {
+        child: Box<Node>,
+        operator: fn(&Value) -> Value,
+    }
+
 }
 
 pub struct Error {
