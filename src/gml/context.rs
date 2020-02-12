3426e82fc5de85e8f8f2c80210021f6a9631eb0e
GM8Emulator <gm8emulator@gmail.com>
2020-02-12 15:50:03 +0000

    gml: add context struct

---
+++ src/gml/context.rs
@@ -0,0 +1,38 @@
+use crate::{
+    gml::Value,
+    instance::{Field, Instance},
+    
+};
+use std::collections::HashMap;
+
+pub struct Context<'a> {
+    /// Reference to the "self" instance
+    pub this: &'a Instance,
+
+    /// Reference to the "other" instance
+    pub other: &'a Instance,
+
+    /// Index of the action currently being executed
+    /// TODO: does this start at 0 or 1?
+    pub event_action: usize,
+
+    /// Type of event (0-11) currently being executed
+    pub event_type: usize,
+
+    /// Sub-event ID
+    pub event_number: usize,
+
+    /// The ID of the object which owns this event - note that this isn't necessarily the same as
+    /// self.object_index, as the event could have been inherited from a parent object
+    pub event_object: u32,
+
+    /// Arguments passed to scripts and such
+    pub arguments: &'a [Value],
+
+    /// Local variables specific to this context
+    /// TODO: replace this with a dummy field-holder object? Global behaves the same way.
+    pub locals: HashMap<usize, Field>,
+
+    /// Return value from this execution - should be initialized to zero as it won't necessarily be written
+    pub return_value: Value,
+}
