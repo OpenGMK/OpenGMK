7ea97c455549051e8ac0a692437a2c50211d187e
GM8Emulator <gm8emulator@gmail.com>
2020-02-27 17:15:53 +0000

    action: partially impl Tree

--- src/action.rs
+++ src/action.rs
@@ -1,4 +1,12 @@
-use crate::gml::{Context, runtime::{Instruction, Node, Runtime}, Value};
+use crate::gml::{compiler::mappings, Context, runtime::{Instruction, Node, Runtime}, Value};
+use gm8exe::asset::etc::CodeAction;
+
+/// Consts which match those used in GM8
+pub mod execution_type {
+    pub const NONE: u32 = 0;
+    pub const FUNCTION: u32 = 1;
+    pub const CODE: u32 = 2;
+}
 
 /// A drag-n-drop action.
 pub struct Action {
@@ -25,6 +33,10 @@ pub struct Action {
     pub if_else: Option<(Box<[Action]>, Box<[Action]>)>,
 }
 
+/// Abstraction for a tree of Actions
+/// Note that Vec is necessary here due to functions such as object_event_add and object_event_clear
+pub struct Tree(Vec<Action>);
+
 pub enum Body {
     Function(fn(&mut Runtime, &mut Context, &[Value]) -> Value),
     Code(Vec<Instruction>),
@@ -33,4 +45,38 @@ pub enum Body {
 pub enum Argument {
     Constant(Value),
     Expression(Node),
+}
+
+impl Tree {
+    /// Turn a list of gm8exe CodeActions into an Action tree.
+    pub fn from_list(list: &[CodeAction]) -> Result<Self, String> {
+        let mut iter = list.iter().peekable().enumerate();
+        let mut output = Vec::new();
+
+        while let Some((i, action)) = iter.next() {
+            match action.execution_type {
+                execution_type::NONE => (),
+                execution_type::FUNCTION => {
+                    if let Some((_, f_ptr, _)) = mappings::FUNCTIONS.iter().find(|(n, _, _)| n == &action.fn_name) {
+                        output.push(Action {
+                            index: i,
+                            target: if action.applies_to_something {Some(action.applies_to)} else {None},
+                            args: Box::new([]),
+                            relative: action.is_relative,
+                            invert_condition: action.invert_condition,
+                            body: Body::Function(*f_ptr),
+                            if_else: None, // TODO: handle action.is_condition
+                        });
+                    } else {
+                        return Err(format!("Unknown function: {} in action {}", action.fn_name, i));
+                    }
+                },
+                execution_type::CODE | _ => {
+                    // TODO: handle code actions
+                },
+            }
+        }
+
+        Ok(Self(output))
+    }
 }
