1cb089916b7823832e6943db2e352ff235bbd8fe
Adam <gm8emulator@gmail.com>
2020-03-09 00:04:26 +0000

    main: add tile struct + module

---
+++ src/tile.rs
@@ -0,0 +1,44 @@
+/// An instance of a background tile.
+pub struct Tile {
+    /// The tile's x position in the room
+    pub x: f64,
+
+    /// The tile's y position in the room
+    pub y: f64,
+
+    /// Index of the background which this tile will draw a section of
+    pub background_index: i32,
+
+    /// The x coordinate to draw from in the background image
+    pub tile_x: i32,
+
+    /// The y coordinate to draw from in the background image
+    pub tile_y: i32,
+
+    /// Width of the tile, in both the background image and the room
+    pub width: i32,
+
+    /// Height of the tile, in both the background image and the room
+    pub height: i32,
+
+    /// Depth of this tile in the room
+    pub depth: i32,
+
+    /// Unique ID of this tile - tile IDs are above 10,000,000
+    pub id: usize,
+
+    /// Alpha value of this tile, from 0.0 (invisible) to 1.0 (opaque)
+    pub alpha: f64,
+
+    /// Colour blend value of this tile
+    pub blend: u32,
+
+    /// xscale with which to draw this tile
+    pub xscale: f64,
+
+    /// yscale with which to draw this tile
+    pub yscale: f64,
+
+    /// Whether this tile will be drawn
+    pub visible: bool,
+}
