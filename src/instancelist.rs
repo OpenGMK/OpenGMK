f8c22f0560b0444d10e979a2381942a95ae4820d
viri <git@stuff.moe>
2020-03-13 09:52:00 -0600

    instancelist: move InstanceChunk to Chunk<T>

--- src/instancelist.rs
+++ src/instancelist.rs
@@ -1,75 +1,15 @@
 use crate::instance::Instance;
 
-const INSTANCES_PER_CHUNK: usize = 256;
+const CHUNK_SIZE: usize = 256;
 
-struct ChunkList<T>(Vec<InstanceChunk<T>>);
+struct ChunkList<T>(Vec<Chunk<T>>);
 
-impl<T> ChunkList<T> {
-    fn new() -> Self {
-        Self(vec![InstanceChunk::new()])
-    }
-
-    fn get(&self, idx: usize) -> Option<&T> {
-        // Calculating these right next to each other guarantees they'll be optimized to a single div op.
-        let idx_div = idx / INSTANCES_PER_CHUNK;
-        let idx_mod = idx % INSTANCES_PER_CHUNK;
-        self.0.get(idx_div).and_then(|chunk| chunk.slots.get(idx_mod)).and_then(|slot| slot.as_ref())
-    }
-
-    fn insert(&mut self, t: T) -> usize {
-        match self.0.iter_mut().enumerate().find(|(_, chunk)| chunk.vacant != 0) {
-            Some((idx, chunk)) => {
-                chunk.vacant -= 1;
-                match chunk.slots.iter_mut().enumerate().find(|(_, slot)| slot.is_none()) {
-                    Some((slot_idx, slot @ None)) => {
-                        *slot = Some(t);
-                        (idx * INSTANCES_PER_CHUNK) + slot_idx
-                    },
-                    _ => unreachable!(),
-                }
-            },
-            None => {
-                let mut chunk = InstanceChunk::new();
-                chunk.vacant -= 1;
-                chunk.slots[0] = Some(t);
-                self.0.push(chunk);
-                (self.0.len() - 1) * INSTANCES_PER_CHUNK
-            },
-        }
-    }
-
-    fn iter(&self) -> impl Iterator<Item = &InstanceChunk<T>> {
-        self.0.iter()
-    }
-
-    fn iter_mut(&mut self) -> impl Iterator<Item = &mut InstanceChunk<T>> {
-        self.0.iter_mut()
-    }
-}
-
-pub struct InstanceList {
-    chunks: ChunkList<Instance>,
-    order: Vec<usize>,
-    order_depth: Vec<usize>,
-}
-
-pub struct Iter {
-    order_idx: usize,
-}
-
-impl Iter {
-    pub fn next(&mut self, list: &InstanceList) -> Option<usize> {
-        self.order_idx += 1;
-        list.order.get(self.order_idx).copied()
-    }
-}
-
-struct InstanceChunk<T> {
-    slots: Box<[Option<T>; INSTANCES_PER_CHUNK]>,
+struct Chunk<T> {
+    slots: Box<[Option<T>; CHUNK_SIZE]>,
     vacant: usize,
 }
 
-impl<T> InstanceChunk<T> {
+impl<T> Chunk<T> {
     pub fn new() -> Self {
         Self {
             // TODO: fix this, please
@@ -91,8 +31,68 @@ impl<T> InstanceChunk<T> {
                 None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
                 None,
             ]),
-            vacant: INSTANCES_PER_CHUNK,
+            vacant: CHUNK_SIZE,
         }
+    }
+}
+
+impl<T> ChunkList<T> {
+    fn new() -> Self {
+        Self(vec![Chunk::new()])
+    }
+
+    fn get(&self, idx: usize) -> Option<&T> {
+        // Calculating these right next to each other guarantees they'll be optimized to a single div op.
+        let idx_div = idx / CHUNK_SIZE;
+        let idx_mod = idx % CHUNK_SIZE;
+        self.0.get(idx_div).and_then(|chunk| chunk.slots.get(idx_mod)).and_then(|slot| slot.as_ref())
+    }
+
+    fn insert(&mut self, t: T) -> usize {
+        match self.0.iter_mut().enumerate().find(|(_, chunk)| chunk.vacant != 0) {
+            Some((idx, chunk)) => {
+                chunk.vacant -= 1;
+                match chunk.slots.iter_mut().enumerate().find(|(_, slot)| slot.is_none()) {
+                    Some((slot_idx, slot @ None)) => {
+                        *slot = Some(t);
+                        (idx * CHUNK_SIZE) + slot_idx
+                    },
+                    _ => unreachable!(),
+                }
+            },
+            None => {
+                let mut chunk = Chunk::new();
+                chunk.vacant -= 1;
+                chunk.slots[0] = Some(t);
+                self.0.push(chunk);
+                (self.0.len() - 1) * CHUNK_SIZE
+            },
+        }
+    }
+
+    fn iter(&self) -> impl Iterator<Item = &Chunk<T>> {
+        self.0.iter()
+    }
+
+    fn iter_mut(&mut self) -> impl Iterator<Item = &mut Chunk<T>> {
+        self.0.iter_mut()
+    }
+}
+
+pub struct InstanceList {
+    chunks: ChunkList<Instance>,
+    order: Vec<usize>,
+    order_depth: Vec<usize>,
+}
+
+pub struct Iter {
+    order_idx: usize,
+}
+
+impl Iter {
+    pub fn next(&mut self, list: &InstanceList) -> Option<usize> {
+        self.order_idx += 1;
+        list.order.get(self.order_idx).copied()
     }
 }
 
