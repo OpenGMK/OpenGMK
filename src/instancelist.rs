ab574af8bca2c7f3a197bd5b569d0bf7d5230fe3
GM8Emulator <gm8emulator@gmail.com>
2020-03-13 14:50:05 +0000

    instancelist: proposed fix for remove_with bug

--- src/instancelist.rs
+++ src/instancelist.rs
@@ -2,30 +2,56 @@ use crate::instance::Instance;
 
 const INSTANCES_PER_CHUNK: usize = 256;
 
-fn chunk_insert<'a, T>(chunks: &'a mut Vec<InstanceChunk<T>>, t: T) -> usize {
-    match chunks.iter_mut().enumerate().find(|(_, chunk)| chunk.vacant != 0) {
-        Some((idx, chunk)) => {
-            chunk.vacant -= 1;
-            match chunk.slots.iter_mut().enumerate().find(|(_, slot)| slot.is_none()) {
-                Some((slot_idx, slot @ None)) => {
-                    *slot = Some(t);
-                    (idx * INSTANCES_PER_CHUNK) + slot_idx
-                },
-                _ => unreachable!(),
-            }
-        },
-        None => {
-            let mut chunk = InstanceChunk::new();
-            chunk.vacant -= 1;
-            chunk.slots[0] = Some(t);
-            chunks.push(chunk);
-            (chunks.len() - 1) * INSTANCES_PER_CHUNK
-        },
+struct ChunkList<T> (Vec<InstanceChunk<T>>);
+
+impl<T> ChunkList<T> {
+    fn new() -> Self {
+        Self(vec![InstanceChunk::new()])
+    }
+
+    fn get(&self, idx: usize) -> Option<&T> {
+        // Calculating these right next to each other guarantees they'll be optimized to a single div op.
+        let idx_div = idx / INSTANCES_PER_CHUNK;
+        let idx_mod = idx % INSTANCES_PER_CHUNK;
+        self.0
+            .get(idx_div)
+            .and_then(|chunk| chunk.slots.get(idx_mod))
+            .and_then(|slot| slot.as_ref())
+    }
+
+    fn insert(&mut self, t: T) -> usize {
+        match self.0.iter_mut().enumerate().find(|(_, chunk)| chunk.vacant != 0) {
+            Some((idx, chunk)) => {
+                chunk.vacant -= 1;
+                match chunk.slots.iter_mut().enumerate().find(|(_, slot)| slot.is_none()) {
+                    Some((slot_idx, slot @ None)) => {
+                        *slot = Some(t);
+                        (idx * INSTANCES_PER_CHUNK) + slot_idx
+                    },
+                    _ => unreachable!(),
+                }
+            },
+            None => {
+                let mut chunk = InstanceChunk::new();
+                chunk.vacant -= 1;
+                chunk.slots[0] = Some(t);
+                self.0.push(chunk);
+                (self.0.len() - 1) * INSTANCES_PER_CHUNK
+            },
+        }
+    }
+
+    fn iter(&self) -> impl Iterator<Item = &InstanceChunk<T>> {
+        self.0.iter()
+    }
+
+    fn iter_mut(&mut self) -> impl Iterator<Item = &mut InstanceChunk<T>> {
+        self.0.iter_mut()
     }
 }
 
 pub struct InstanceList {
-    chunks: Vec<InstanceChunk<Instance>>,
+    chunks: ChunkList<Instance>,
     order: Vec<usize>,
     order_depth: Vec<usize>,
 }
@@ -75,15 +101,11 @@ impl<T> InstanceChunk<T> {
 
 impl InstanceList {
     pub fn new() -> Self {
-        Self { chunks: vec![InstanceChunk::new()], order: Vec::new(), order_depth: Vec::new() }
+        Self { chunks: ChunkList::new(), order: Vec::new(), order_depth: Vec::new() }
     }
 
     pub fn get(&self, idx: usize) -> Option<&Instance> {
-        // TODO: does LLVM optimize out div/mod ?
-        self.chunks
-            .get(idx / INSTANCES_PER_CHUNK)
-            .and_then(|chunk| chunk.slots.get(idx % INSTANCES_PER_CHUNK))
-            .and_then(|slot| slot.as_ref())
+        self.chunks.get(idx)
     }
 
     pub fn iter(&self) -> Iter {
@@ -91,14 +113,14 @@ impl InstanceList {
     }
 
     pub fn insert(&mut self, instance: Instance) -> usize {
-        let value = chunk_insert(&mut self.chunks, instance);
+        let value = self.chunks.insert(instance);
         self.order.push(value);
         self.order_depth.push(value);
         value
     }
 
     pub fn remove_with(&mut self, f: impl Fn(&Instance) -> bool) {
-        for chunk in &mut self.chunks {
+        for chunk in self.chunks.iter_mut() {
             for slot in chunk.slots.iter_mut() {
                 if let Some(instance) = slot {
                     if f(&*instance) {
@@ -108,7 +130,8 @@ impl InstanceList {
                 }
             }
         }
-        let chunks = &mut self.chunks; // borrowck :)
+
+        let chunks = &self.chunks;
         self.order.retain(|idx| chunks.get(*idx).is_some());
     }
 }
