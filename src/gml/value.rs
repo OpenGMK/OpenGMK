120890fad5e00c7e95af24adf772a4072cffd9b6
Adam <gm8emulator@gmail.com>
2020-04-01 14:47:40 +0100

    value: impl From<Value> for i32

--- src/gml/value.rs
+++ src/gml/value.rs
@@ -311,6 +311,16 @@ impl From<Rc<str>> for Value {
     }
 }
 
+impl From<Value> for i32 {
+    // For lazy-converting a value into an i32.
+    fn from(value: Value) -> Self {
+        match value {
+            Value::Real(r) => r as i32,
+            Value::Str(_) => 0,
+        }
+    }
+}
+
 impl Default for Value {
     fn default() -> Self {
         Self::Real(0.0)
