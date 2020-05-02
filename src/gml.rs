ddeebb1d40df56b7b8d3923934b6465fcde2d44c
Adam <gm8emulator@gmail.com>
2020-05-02 21:05:19 +0100

    gml: add comment clarifying -6 for #21

--- src/gml.rs
+++ src/gml.rs
@@ -16,6 +16,11 @@ pub use runtime::Error;
 pub const TRUE: f64 = 1.0;
 pub const FALSE: f64 = 0.0;
 
+// Note: -6 is *not* a special reference in GameMaker 8, and is listed as "Not Specified" in their documentation:
+// https://docs.yoyogames.com/source/dadiospice/002_reference/debugging/runner%20errors.html
+// Dereferencing -6 produces an error message, just like any other negative value beyond -7. It's not clear why YYG
+// decided to skip -6 but, regardless, that is why there's no special name for it here.
+
 pub const SELF: i32 = -1;
 pub const OTHER: i32 = -2;
 pub const ALL: i32 = -3;
