cb161b9f92031bb53304118ce23da1f572c86230
Adam <gm8emulator@gmail.com>
2020-05-16 18:46:19 +0100

    replay: better length calculation

--- src/replay.rs
+++ src/replay.rs
@@ -1,13 +1,8 @@
-use std::collections::HashMap;
 use crate::input::{Key, MouseButton};
 
 // Represents an entire replay (TAS) file
 #[derive(Debug)]
 pub struct Replay {
-    // The different room speeds that are used in this replay, and how many frames of each.
-    // Mainly used for calculating the real-time length with minimal floating point errors.
-    speed_map: HashMap<u32, usize>,
-
     // System time to use at the beginning of this replay.
     // Will be used to spoof some GML variables such as `current_time`.
     pub start_time: u128,
@@ -22,6 +17,7 @@ pub struct Replay {
 // Associated data for a single frame of playback
 #[derive(Debug)]
 pub struct Frame {
+    pub fps: u32,
     pub mouse_x: i32,
     pub mouse_y: i32,
     pub inputs: Vec<Input>,
@@ -41,7 +37,6 @@ pub enum Input {
 impl Replay {
     pub fn new(start_time: u128, start_seed: i32) -> Self {
         Self {
-            speed_map: HashMap::new(),
             start_time,
             start_seed,
             frames: Vec::new(),
@@ -51,12 +46,13 @@ impl Replay {
     // Adds a new frame of input to the end of the replay.
     // Mouse position will be the same as the previous frame unless this is the first frame,
     // in which case it will be (0, 0)
-    pub fn add_frame(&mut self) -> &mut Frame {
+    pub fn add_frame(&mut self, fps: u32) -> &mut Frame {
         let (mouse_x, mouse_y) = match self.frames.last() {
             Some(frame) => (frame.mouse_x, frame.mouse_y),
             None => (0, 0),
         };
         self.frames.push(Frame {
+            fps,
             mouse_x,
             mouse_y,
             inputs: Vec::new(),
@@ -69,8 +65,28 @@ impl Replay {
         self.frames.get(index)
     }
 
-    // Calculates the length of this replay in seconds
+    // Calculates the length of this replay in milliseconds
     pub fn get_length(&self) -> f64 {
-        self.speed_map.iter().map(|(&fps, &count)| (count as f64) / f64::from(fps)).sum()
+        // We want to do this in a way that'll avoid FPI as much as possible (for example in a 60FPS game)
+        let mut ms: f64 = 0.0;
+
+        let mut iter = self.frames.iter().peekable();
+        loop {
+            let speed = match iter.next() {
+                Some(s) => s.fps,
+                None => break,
+            };
+            let mut count: usize = 1;
+            while let Some(Frame { fps, .. }) = iter.peek() {
+                if *fps == speed {
+                    iter.next();
+                    count += 1;
+                } else {
+                    break
+                }
+            }
+            ms += (count * 1000) as f64 / (speed as f64)
+        }
+        ms
     }
 }
