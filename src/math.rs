5120448bf0335d0634bd9db875fc428a5bdb0bcd
viri <git@stuff.moe>
2020-05-19 19:40:55 -0600

    math: impl From<i32> for Real

--- src/math.rs
+++ src/math.rs
@@ -1,9 +1,15 @@
-use std::{hint::black_box, ops::Add};
+use std::{hint::black_box, ops::{Add, Sub}};
 
 /// A transparent wrapper for f64 with extended precision (80-bit) arithmetic.
 #[derive(Copy, Clone, Default)]
 #[repr(transparent)]
 pub struct Real(f64);
+
+impl From<i32> for Real {
+    fn from(i: i32) -> Self {
+        Real(f64::from(i))
+    }
+}
 
 impl From<f64> for Real {
     fn from(f: f64) -> Self {
