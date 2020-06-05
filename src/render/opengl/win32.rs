053b4783316c86cec77c4cf7a350d0c3fe9b13f7
viri <git@stuff.moe>
2020-06-05 01:13:20 -0600

    opengl(win32): redo all the ugly stuff

--- src/render/opengl/win32.rs
+++ src/render/opengl/win32.rs
@@ -3,7 +3,8 @@
 use super::gl;
 use crate::game::window::win32::WindowImpl;
 use std::{
-    mem::{size_of, transmute},
+    ffi::CStr,
+    mem::{self, size_of, transmute},
     os::raw::{c_char, c_int, c_void},
     ptr,
 };
@@ -22,6 +23,11 @@ use winapi::{
         winuser::GetDC,
     },
 };
+
+#[allow(clippy::all)]
+pub mod wgl {
+    include!(concat!(env!("OUT_DIR"), "/wgl_bindings.rs"));
+}
 
 pub struct PlatformGL {
     hdc: HDC,
@@ -57,22 +63,14 @@ static PIXEL_FORMAT: PIXELFORMATDESCRIPTOR = PIXELFORMATDESCRIPTOR {
     dwDamageMask: 0,
 };
 
-const WGL_CONTEXT_MAJOR_VERSION_ARB: c_int = 0x2091;
-const WGL_CONTEXT_MINOR_VERSION_ARB: c_int = 0x2092;
-const WGL_CONTEXT_LAYER_PLANE_ARB: c_int = 0x2093;
-const WGL_CONTEXT_FLAGS_ARB: c_int = 0x2094;
-const WGL_CONTEXT_PROFILE_MASK_ARB: c_int = 0x9126;
-const WGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB: c_int = 0x0002;
-const WGL_CONTEXT_CORE_PROFILE_BIT_ARB: c_int = 0x0001;
-
 #[rustfmt::skip]
 /// Flags for wglCreateContextAttribsARB
-static WGL_CCTX_ATTR_ARB: &[c_int] = &[
-    WGL_CONTEXT_MAJOR_VERSION_ARB, 3,
-    WGL_CONTEXT_MINOR_VERSION_ARB, 3,
-    WGL_CONTEXT_LAYER_PLANE_ARB,   0,
-    WGL_CONTEXT_FLAGS_ARB,         WGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB,
-    WGL_CONTEXT_PROFILE_MASK_ARB,  WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
+static WGL_CCTX_ATTR_ARB: &[u32] = &[
+    wgl::CONTEXT_MAJOR_VERSION_ARB, 3,
+    wgl::CONTEXT_MINOR_VERSION_ARB, 3,
+    wgl::CONTEXT_LAYER_PLANE_ARB,   0,
+    wgl::CONTEXT_FLAGS_ARB,         wgl::CONTEXT_FORWARD_COMPATIBLE_BIT_ARB,
+    wgl::CONTEXT_PROFILE_MASK_ARB,  wgl::CONTEXT_CORE_PROFILE_BIT_ARB,
     0, // END
 ];
 
@@ -101,7 +99,7 @@ impl PlatformGL {
         let result = SetPixelFormat(device, format, &PIXEL_FORMAT);
         assert_ne!(result, 0, "couldn't set pixel format");
 
-        // for 2.0+ functions
+        // for 1.1 functions
         let gl32 = LoadLibraryA(b"opengl32.dll\0".as_ptr() as *const c_char);
 
         // create OpenGL context for opening ARB context
@@ -109,33 +107,40 @@ impl PlatformGL {
         let result = wglMakeCurrent(device, context);
         assert_ne!(result, 0, "couldn't make new context current");
 
-        // create the real context! (TODO: this should be fine to avoid if it's already 3.2 Core?)
-        let wgl_cctx = b"wglCreateContextAttribsARB\0".as_ptr() as *const c_char;
-        let wgl_cctx_fn = load_gl_function(wgl_cctx, gl32);
-        assert!(!wgl_cctx_fn.is_null(), "can't load wglCCtxARB");
-        let context2 = transmute::<_, WglCreateContextAttribsARBTy>(wgl_cctx_fn)(
-            device,
-            ptr::null_mut(),
-            WGL_CCTX_ATTR_ARB as *const _ as *const c_int,
-        );
-        assert!(!context2.is_null(), "wglCCtxARB returned null");
-        wglMakeCurrent(device, context2);
-        wglDeleteContext(context);
-
-        // load function pointers
         static mut FFI_BUF: Vec<u8> = Vec::new();
         FFI_BUF.reserve(64);
-        gl::load_with(|name| {
+        let loader_fn = |name: &str| {
             FFI_BUF.clear();
             FFI_BUF.extend_from_slice(name.as_bytes());
             FFI_BUF.push(0);
             load_gl_function(FFI_BUF.as_ptr() as *const c_char, gl32)
-        });
+        };
 
-        let ver_str = std::ffi::CStr::from_ptr(gl::GetString(gl::VERSION) as *const _).to_str().unwrap();
+        // create the real context! (TODO: this should be fine to avoid if it's already 3.2?)
+        wgl::load_with(loader_fn);
+        if !wgl::CreateContextAttribsARB::is_loaded() {
+            panic!("wglCreateContextAttribsARB not loaded! Uh oh...");
+        }
+        let context2 = wgl::CreateContextAttribsARB(
+            device as *const c_void,
+            ptr::null_mut(),
+            WGL_CCTX_ATTR_ARB as *const _ as *const c_int,
+        ) as HGLRC;
+        assert!(!context2.is_null(), "wglCreateContextAttribsARB returned null");
+        wglMakeCurrent(device, context2);
+        wglDeleteContext(context);
+
+        // load gl function pointers
+        gl::load_with(loader_fn);
+
+        // debug print, whatever
+        let ver_str = CStr::from_ptr(gl::GetString(gl::VERSION) as *const _).to_str().unwrap();
         println!("OpenGL Version: {}", ver_str);
-        let vendor_str = std::ffi::CStr::from_ptr(gl::GetString(gl::VENDOR) as *const _).to_str().unwrap();
+        let vendor_str = CStr::from_ptr(gl::GetString(gl::VENDOR) as *const _).to_str().unwrap();
         println!("OpenGL Vendor: {}", vendor_str);
+
+        // don't leak memory
+        let _ = mem::replace(&mut FFI_BUF, Vec::new());
 
         PlatformGL { hdc: device, hglrc: context2 }
     }
@@ -146,10 +151,11 @@ impl PlatformGL {
     }
 
     pub unsafe fn swap_interval(&self, n: u32) {
-        let gl32 = LoadLibraryA(b"opengl32.dll\0".as_ptr() as *const c_char);
-        let fp = load_gl_function(b"wglSwapIntervalEXT\0".as_ptr() as *const c_char, gl32);
-        assert!(!fp.is_null(), "wglSwapIntervalEXT was not found");
-        transmute::<_, extern "C" fn(c_int) -> BOOL>(fp)(n as c_int);
+        if wgl::SwapIntervalEXT::is_loaded() {
+            wgl::SwapIntervalEXT(n as i32);
+        } else {
+            eprintln!("wglSwapIntervalEXT missing!");
+        }
     }
 
     #[must_use]
