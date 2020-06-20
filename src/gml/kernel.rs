7e343a8cc5fab8455f96f960e72caf8b7d8c3cfb
Adam <gm8emulator@gmail.com>
2020-06-20 18:36:50 +0100

    kernel: action_draw_health

--- src/gml/kernel.rs
+++ src/gml/kernel.rs
@@ -2084,10 +2084,103 @@ impl Game {
         .into())
     }
 
-    pub fn action_draw_health(&mut self, _context: &mut Context, _args: &[Value]) -> gml::Result<Value> {
-        // Expected arg count: 6
-        // TODO
-        //unimplemented!("Called unimplemented kernel function action_draw_health")
+    pub fn action_draw_health(&mut self, context: &mut Context, args: &[Value]) -> gml::Result<Value> {
+        let (mut x1, mut y1, mut x2, mut y2, back_col, col) = expect_args!(args, [real, real, real, real, int, int])?;
+
+        if context.relative {
+            let instance = self.instance_list.get(context.this);
+            let x = instance.x.get();
+            let y = instance.y.get();
+            x1 += x;
+            x2 += x;
+            y1 += y;
+            y2 += y;
+        }
+
+        use mappings::constants;
+        let (bar_colour, bar_colour_to) = match col {
+            0 => (constants::C_GREEN, Some(constants::C_RED)),
+            1 => (constants::C_WHITE, Some(constants::C_BLACK)),
+            2 => (constants::C_BLACK, None),
+            3 => (constants::C_GRAY, None),
+            4 => (constants::C_SILVER, None),
+            5 => (constants::C_WHITE, None),
+            6 => (constants::C_MAROON, None),
+            7 => (constants::C_GREEN, None),
+            8 => (constants::C_OLIVE, None),
+            9 => (constants::C_NAVY, None),
+            10 => (constants::C_PURPLE, None),
+            11 => (constants::C_TEAL, None),
+            12 => (constants::C_RED, None),
+            13 => (constants::C_LIME, None),
+            14 => (constants::C_YELLOW, None),
+            15 => (constants::C_BLUE, None),
+            16 => (constants::C_FUCHSIA, None),
+            17 => (constants::C_AQUA, None),
+            _ => (constants::C_BLACK, None),
+        };
+        let back_colour = match back_col {
+            0 => None,
+            1 => Some(constants::C_BLACK),
+            2 => Some(constants::C_GRAY),
+            3 => Some(constants::C_SILVER),
+            4 => Some(constants::C_WHITE),
+            5 => Some(constants::C_MAROON),
+            6 => Some(constants::C_GREEN),
+            7 => Some(constants::C_OLIVE),
+            8 => Some(constants::C_NAVY),
+            9 => Some(constants::C_PURPLE),
+            10 => Some(constants::C_TEAL),
+            11 => Some(constants::C_RED),
+            12 => Some(constants::C_LIME),
+            13 => Some(constants::C_YELLOW),
+            14 => Some(constants::C_BLUE),
+            15 => Some(constants::C_FUCHSIA),
+            16 => Some(constants::C_AQUA),
+            _ => Some(constants::C_PURPLE),
+        };
+
+        let health_ratio = f64::from(self.health / Real::from(100.0));
+
+        fn lerp_u8(from: u8, to: u8, ratio: f64) -> u8 {
+            if from <= to {
+                from + ((to - from) as f64 * ratio) as u8
+            } else {
+                from - ((from - to) as f64 * ratio) as u8
+            }
+        }
+
+        let bar_colour = if let Some(c_to) = bar_colour_to {
+            // No, these aren't backwards. "from green to red" actually means we want a fully-green bar
+            // at full health, so we are actually lerping from red to green by scalar health.
+            let bytes_to = (bar_colour as i32).to_le_bytes();
+            let bytes_from = (c_to as i32).to_le_bytes();
+            i32::from_le_bytes([
+                lerp_u8(bytes_from[0], bytes_to[0], health_ratio),
+                lerp_u8(bytes_from[1], bytes_to[1], health_ratio),
+                lerp_u8(bytes_from[1], bytes_to[2], health_ratio),
+                0,
+            ])
+        } else {
+            bar_colour as i32
+        };
+
+        if let Some(colour) = back_colour {
+            self.renderer.draw_rectangle(
+                x1.into(),
+                y1.into(),
+                x2.into(),
+                y2.into(),
+                colour as i32,
+                self.draw_alpha.into(),
+            );
+            self.renderer.draw_rectangle_outline(x1.into(), y1.into(), x2.into(), y2.into(), 0, self.draw_alpha.into());
+        }
+
+        x2 = x1 + ((x2 - x1) * health_ratio.into());
+        self.renderer.draw_rectangle(x1.into(), y1.into(), x2.into(), y2.into(), bar_colour, self.draw_alpha.into());
+        self.renderer.draw_rectangle_outline(x1.into(), y1.into(), x2.into(), y2.into(), 0, self.draw_alpha.into());
+
         Ok(Default::default())
     }
 
