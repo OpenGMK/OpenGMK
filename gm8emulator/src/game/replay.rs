33b8ec75fa9a5eb8aa59c21c532dfce0a4e114ae
Adam <gm8emulator@gmail.com>
2020-06-29 18:39:14 +0100

    replay: add new_seed, new_time per frame

--- gm8emulator/src/game/replay.rs
+++ gm8emulator/src/game/replay.rs
@@ -24,6 +24,8 @@ pub struct Frame {
     pub mouse_y: f64,
     pub inputs: Vec<Input>,
     pub events: Vec<Event>,
+    pub new_seed: Option<i32>,
+    pub new_time: Option<u128>,
 }
 
 // Stored events for certain things which must always happen the same way during replay
@@ -61,7 +63,15 @@ impl Replay {
             Some(frame) => (frame.mouse_x, frame.mouse_y),
             None => (0.0, 0.0),
         };
-        self.frames.push(Frame { fps, mouse_x, mouse_y, inputs: Vec::new(), events: Vec::new() });
+        self.frames.push(Frame {
+            fps,
+            mouse_x,
+            mouse_y,
+            inputs: Vec::new(),
+            events: Vec::new(),
+            new_seed: None,
+            new_time: None,
+        });
         self.frames.last_mut().unwrap() // Last cannot be None since we just pushed an element
     }
 
