f9bcbae5de0017d4f9a45776f6101a14d485d6fd
Floogle <18466542+skyfloogle@users.noreply.github.com>
2020-07-13 20:56:55 +0200

    control-panel: add new RendererOptions option

--- control-panel/panel.rs
+++ control-panel/panel.rs
@@ -186,7 +186,7 @@ impl ControlPanel {
         let clear_colour = Colour::new(220.0 / 255.0, 220.0 / 255.0, 220.0 / 255.0);
         let mut renderer = Renderer::new(
             (),
-            &RendererOptions { size: (WINDOW_WIDTH, WINDOW_HEIGHT), vsync: true },
+            &RendererOptions { size: (WINDOW_WIDTH, WINDOW_HEIGHT), vsync: true, interpolate_pixels: false },
             &window,
             clear_colour,
         )?;
