ae56a8be402c5546b2f999e317c9770f2f9b46f5
Floogle <18466542+skyfloogle@users.noreply.github.com>
2020-09-12 13:15:27 +0200

    main: replace tempdir arg with search in projpath

--- gm8emulator/src/main.rs
+++ gm8emulator/src/main.rs
@@ -46,7 +46,6 @@ fn xmain() -> i32 {
     opts.optflag("t", "singlethread", "parse gamedata synchronously");
     opts.optflag("v", "verbose", "enables verbose logging");
     opts.optflag("r", "realtime", "disables clock spoofing");
-    opts.optopt("tmp", "tempdir", "directory to store temporary files in", "DIRECTORY");
     opts.optopt("p", "port", "port to open for external game control (default 15560)", "PORT");
     opts.optopt("n", "project-name", "name of TAS project to create or load", "NAME");
     opts.optopt("f", "replay-file", "path to savestate file to replay", "FILE");
@@ -76,17 +75,6 @@ fn xmain() -> i32 {
     let multithread = !matches.opt_present("t");
     let spoof_time = !matches.opt_present("r");
     let verbose = matches.opt_present("v");
-    let temp_dir = matches.opt_str("tmp").map(|path| {
-        let path = PathBuf::from(path);
-        if !path.is_dir() {
-            if path.exists() {
-                panic!("temp directory exists but is not a directory");
-            } else {
-                std::fs::create_dir_all(&path).unwrap();
-            }
-        }
-        path
-    });
     let port = match matches.opt_str("p").map(|x| x.parse::<u16>()).transpose() {
         Ok(p) => p,
         Err(e) => {
@@ -100,6 +88,21 @@ fn xmain() -> i32 {
         p.push("projects");
         p.push(name);
         p
+    });
+    let temp_dir = project_path.as_ref().map(|proj_path| {
+        // attempt to find temp dir in project path
+        std::fs::read_dir(proj_path)
+            .ok()
+            .and_then(|iter| {
+                iter.filter_map(|x| x.ok())
+                    .find(|p| {
+                        p.metadata().ok().filter(|p| p.is_dir()).is_some()
+                            && p.file_name().to_str().filter(|s| s.starts_with("gm_ttt_")).is_some()
+                    })
+                    .map(|entry| entry.path())
+            })
+            // if we can't find one, make one
+            .unwrap_or_else(|| format!("gm_ttt_{:.0}", rand::random::<f64>().fract() * 99999.0).into())
     });
     let replay = matches.opt_str("f").map(|filename| {
         let mut filepath = PathBuf::from(&filename);
