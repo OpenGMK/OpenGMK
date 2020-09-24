36e356c47ae9db690196bf6b21ea0bb335e40142
Floogle <18466542+skyfloogle@users.noreply.github.com>
2020-09-25 00:28:34 +0200

    opengl: draw_rectangle no longer wraps draw_sprite

--- gmio/src/render/opengl.rs
+++ gmio/src/render/opengl.rs
@@ -1479,7 +1479,14 @@ impl RendererTrait for RendererImpl {
         let copied_pixel = self.white_pixel;
         let x2 = if x2 == x2.floor() { x2 + 0.01 } else { x2 };
         let y2 = if y2 == y2.floor() { y2 + 0.01 } else { y2 };
-        self.draw_sprite(&copied_pixel, x1 + 0.5, y1 + 0.5, x2 - x1, y2 - y1, 0.0, colour, alpha)
+        self.push_primitive(
+            ShapeBuilder::new(false, self.white_pixel, alpha, self.depth)
+                .push_point(x1, y1, colour)
+                .push_point(x2, y1, colour)
+                .push_point(x2, y2, colour)
+                .push_point(x1, y2, colour)
+                .build(),
+        );
     }
 
     fn draw_rectangle_outline(&mut self, x1: f64, y1: f64, x2: f64, y2: f64, colour: i32, alpha: f64) {
