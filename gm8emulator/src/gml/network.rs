05008bf41b2079a788b2f24248aea7e0151cdc17
Floogle <18466542+skyfloogle@users.noreply.github.com>
2020-10-17 20:45:15 +0200

    network: fmt

--- gm8emulator/src/gml/network.rs
+++ gm8emulator/src/gml/network.rs
@@ -13,7 +13,7 @@ pub fn get_local_ip() -> io::Result<net::IpAddr> {
     // check for this because the operating system knows more and better, and we trust it.
     let port = match socket.local_addr() {
         Ok(a) => a.port(),
-        Err(_) => 0,  // this works too at least on Windows 7, so let's try
+        Err(_) => 0, // this works too at least on Windows 7, so let's try
     };
 
     // We're binding socket to INADDR_ANY, so it's necessary to specify a peer in order to
@@ -24,19 +24,17 @@ pub fn get_local_ip() -> io::Result<net::IpAddr> {
         // First of all, we try to determine the address of our machine under which it is
         // known to other machines on our LAN. This is the most usable case because it can
         // be used for multiplayer, as well as it should behave as localhost for ourselves.
-        ([192,168,255,255], port).into(),  // class C private: 192.168.0.0/16
-        ([172,31,255,255], port).into(),  // class B private: 172.16.0.0/12
-        ([10,255,255,255], port).into(),  // class A private: 10.0.0.0/8
-
+        ([192, 168, 255, 255], port).into(), // class C private: 192.168.0.0/16
+        ([172, 31, 255, 255], port).into(),  // class B private: 172.16.0.0/12
+        ([10, 255, 255, 255], port).into(),  // class A private: 10.0.0.0/8
         // See 'INADDR_BROADCAST'. This also handles the virtual network adapters such as
         // ones from VMWare / VirtualBox to imitate the IP lookup based on gethostbyname().
-        (net::Ipv4Addr::BROADCAST, port).into(),  // limited broadcast: 255.255.255.255/32
-
+        (net::Ipv4Addr::BROADCAST, port).into(), // limited broadcast: 255.255.255.255/32
         // If all the previous effort has failed (which is pretty unlikely scenario), we
         // give up and try to obtain at least the correct loopback address of this cursed
         // machine. It almost never differs from the common 127.0.0.1, but in theory it is
         // possible, and if we have already got to this place, anything can happen.
-        ([127,255,255,255], port).into(),  // host loopback: 127.0.0.0/8
+        ([127, 255, 255, 255], port).into(), // host loopback: 127.0.0.0/8
     ];
 
     // Note that there are no real 'connections' in UDP, so what .connect() actually does
