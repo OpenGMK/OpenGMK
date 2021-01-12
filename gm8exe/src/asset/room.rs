QuentinJanuel/GM8Decompiler

gm8exe/asset: rename `AssetDataError` to `Error`

gm8exe/src/asset/room.rs
@@ -1,5 +1,5 @@
 use crate::{
-    asset::{assert_ver, Asset, AssetDataError, PascalString, ReadPascalString, WritePascalString},
+    asset::{assert_ver, Asset, Error, PascalString, ReadPascalString, WritePascalString},
     colour::Colour,
     def::ID,
     GameVersion,
@@ -108,7 +108,7 @@ pub struct ViewFollowData {
 }
 
 impl Asset for Room {
-    fn deserialize<B>(bytes: B, strict: bool, _version: GameVersion) -> Result<Self, AssetDataError>
+    fn deserialize<B>(bytes: B, strict: bool, _version: GameVersion) -> Result<Self, Error>
     where
         B: AsRef<[u8]>,
         Self: Sized,

OpenGMK/OpenGMK

gm8exe/src/asset/room.rs
@@ -1,5 +1,5 @@
 use crate::{
-    asset::{assert_ver, Asset, AssetDataError, PascalString, ReadPascalString, WritePascalString},
+    asset::{assert_ver, Asset, Error, PascalString, ReadPascalString, WritePascalString},
     colour::Colour,
     def::ID,
     GameVersion,
@@ -111,7 +111,7 @@ pub struct ViewFollowData {
 }
 
 impl Asset for Room {
-    fn deserialize<B>(bytes: B, strict: bool, version: GameVersion) -> Result<Self, AssetDataError>
+    fn deserialize<B>(bytes: B, strict: bool, version: GameVersion) -> Result<Self, Error>
     where
         B: AsRef<[u8]>,
         Self: Sized,