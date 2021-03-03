a054be2d5c09c53474d2e0fe6f6993ee63f614e0
viri <hi@viri.moe>
2021-03-03 00:23:32 -0600

    gml-parser: make serde optional (via feature)

--- gml-parser/src/token.rs
+++ gml-parser/src/token.rs
@@ -1,4 +1,6 @@
+#[cfg(feature = "runner-serde-derives")]
 use serde::{Deserialize, Serialize};
+
 use std::fmt;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -36,7 +38,8 @@ pub enum Keyword {
     Exit,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Copy, Debug, PartialEq)]
+#[cfg_attr(feature = "runner-serde-derives", derive(Serialize, Deserialize))]
 pub enum Operator {
     /// `+` Add or unary positive (redundant)
     Add,
