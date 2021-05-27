19d4fd9d5d10c09966ced696667fde28b6c7f20e
viri <hi@viri.moe>
2021-05-27 15:59:58 -0600

    emu/gm_save: import fixes + nop diff
    What on earth?

--- gm8emulator/src/game/gm_save.rs
+++ gm8emulator/src/game/gm_save.rs
@@ -3,9 +3,9 @@ use crate::{
     gml,
     instance::DummyFieldHolder,
     instancelist::{InstanceList, TileList},
+    types::Colour,
 };
 use serde::{Deserialize, Serialize};
-use shared::types::Colour;
 use std::collections::HashSet;
 
 /// A save file for use with game_save() and game_load().
@@ -69,7 +69,7 @@ impl GMSave {
                 persistent: game.room.persistent,
                 bgcol: game.room.colour,
                 show_bgcol: game.room.show_colour,
-                show_windowcol: true, // TODO: 
+                show_windowcol: true, // TODO:
                 backgrounds: game.room.backgrounds.clone(),
                 views_enabled: game.room.views_enabled,
                 views: game.room.views.clone(),
