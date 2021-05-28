4919dc6ffdc0762d4b21e7194fcb4e70a5f22a18
viri <hi@viri.moe>
2021-05-28 17:42:54 -0600

    gmk/asset: add `ByteString` type

---
+++ gmk-parser/src/asset.rs
@@ -0,0 +1,14 @@
+use std::fmt;
+
+/// Represents a GameMaker string which may or may not be valid UTF-8.
+#[derive(Clone)]
+pub struct ByteString(pub Vec<u8>);
+
+impl fmt::Debug for ByteString {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        formatter
+            .debug_tuple("ByteString")
+            .field(&&*String::from_utf8_lossy(self.0.as_slice()))
+            .finish()
+    }
+}
