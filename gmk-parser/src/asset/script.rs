8ba5a61ae1f864ce1bcda1e6d65a86e9fb7d66ce
viri <hi@viri.moe>
2021-05-28 19:30:32 -0600

    gmk/script: full `Asset` impl

--- gmk-parser/src/asset/script.rs
+++ gmk-parser/src/asset/script.rs
@@ -1,6 +1,70 @@
-use crate::asset::ByteString;
+use crate::asset::{Asset, ByteString, Timestamp, Version};
+
+use byteorder::{LE, ReadBytesExt, WriteBytesExt};
+use std::io;
 
 pub struct Script {
     pub name: ByteString,
+    pub timestamp: Timestamp,
+    pub version: Version,
+
     pub source: ByteString,
+}
+
+impl Asset for Script {
+    #[inline]
+    fn name(&self) -> &[u8] {
+        self.name.0.as_slice()
+    }
+
+    #[inline]
+    fn timestamp(&self) -> Timestamp {
+        self.timestamp
+    }
+
+    #[inline]
+    fn version(&self) -> Version {
+        self.version
+    }
+
+    fn from_gmk<R: io::Read>(&self, mut reader: R) -> io::Result<Self> {
+        Self::read(&mut reader, true)
+    }
+
+    fn to_gmk<W: io::Write>(&self, mut writer: W) -> io::Result<()> {
+        self.write(&mut writer, true)
+    }
+
+    fn from_exe<R: io::Read>(&self, mut reader: R) -> io::Result<Self> {
+        Self::read(&mut reader, false)
+    }
+
+    fn to_exe<W: io::Write>(&self, mut writer: W) -> io::Result<()> {
+        self.write(&mut writer, false)
+    }
+}
+
+impl Script {
+    fn read(mut reader: &mut dyn io::Read, is_gmk: bool) -> io::Result<Self> {
+        let name = ByteString::read(&mut reader)?;
+        let timestamp = if is_gmk {
+            Timestamp(reader.read_f64::<LE>()?)
+        } else {
+            Timestamp::default()
+        };
+        let version = read_version!(reader, name, is_gmk, "script", Gm800)?;
+        let source = ByteString::read(&mut reader)?;
+        Ok(Self { name, timestamp, version, source })
+    }
+
+    fn write(&self, mut writer: &mut dyn io::Write, is_gmk: bool) -> io::Result<()> {
+        assert_eq!(self.version, Version::Gm800);
+        self.name.write(&mut writer)?;
+        if is_gmk {
+            writer.write_f64::<LE>(self.timestamp.0)?;
+        }
+        writer.write_u32::<LE>(self.version as u32)?;
+
+        self.source.write(&mut writer)
+    }
 }
