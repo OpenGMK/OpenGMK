e00455ce72a9b800ffa4edee5afe3dc8067c6018
viri <hi@viri.moe>
2021-05-28 18:00:02 -0600

    gmk/asset: add `Asset` trait v3

--- gmk-parser/src/asset.rs
+++ gmk-parser/src/asset.rs
@@ -1,6 +1,17 @@
 mod script;
 
-use std::fmt;
+use byteorder::{LE, ReadBytesExt, WriteBytesExt};
+use std::{convert::TryFrom, fmt, io};
+
+pub trait Asset: Sized {
+    fn name(&self) -> &[u8];
+
+    fn from_gmk<R: io::Read>(&self, r: R) -> io::Result<Self>;
+    fn to_gmk<W: io::Write>(&self, w: W) -> io::Result<()>;
+
+    fn from_exe<R: io::Read>(&self, r: R) -> io::Result<Self>;
+    fn to_exe<W: io::Write>(&self, w: W) -> io::Result<()>;
+}
 
 /// Represents a GameMaker string which may or may not be valid UTF-8.
 #[derive(Clone)]
