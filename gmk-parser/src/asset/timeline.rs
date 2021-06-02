acd8dddfbe1bc2dceab10502f38532eee6eb5f2c
Adam <classygopher@gmail.com>
2021-06-02 15:43:10 +0100

    gmk/timeline: full  implementation

---
+++ gmk-parser/src/asset/timeline.rs
@@ -0,0 +1,94 @@
+use crate::asset::{Action, Asset, ByteString, Timestamp, Version};
+
+use byteorder::{LE, ReadBytesExt, WriteBytesExt};
+use std::io;
+
+pub struct Timeline {
+    pub name: ByteString,
+    pub timestamp: Timestamp,
+    pub version: Version,
+
+    pub moments: Vec<(u32, Vec<Action>)>,
+}
+
+impl Asset for Timeline {
+    #[inline]
+    fn name(&self) -> &[u8] {
+        self.name.0.as_slice()
+    }
+
+    #[inline]
+    fn timestamp(&self) -> Timestamp {
+        self.timestamp
+    }
+
+    #[inline]
+    fn version(&self) -> Version {
+        self.version
+    }
+
+    fn from_gmk<R: io::Read>(&self, mut reader: R) -> io::Result<Self> {
+        Self::read(&mut reader, true)
+    }
+
+    fn to_gmk<W: io::Write>(&self, mut writer: W) -> io::Result<()> {
+        self.write(&mut writer, true)
+    }
+
+    fn from_exe<R: io::Read>(&self, mut reader: R) -> io::Result<Self> {
+        Self::read(&mut reader, false)
+    }
+
+    fn to_exe<W: io::Write>(&self, mut writer: W) -> io::Result<()> {
+        self.write(&mut writer, false)
+    }
+}
+
+impl Timeline {
+    fn read(mut reader: &mut dyn io::Read, is_gmk: bool) -> io::Result<Self> {
+        let name = ByteString::read(&mut reader)?;
+        let timestamp = if is_gmk {
+            Timestamp(reader.read_f64::<LE>()?)
+        } else {
+            Timestamp::default()
+        };
+        let version = read_version!(reader, name, is_gmk, "timeline", Gm500)?;
+
+        let moment_count = reader.read_u32::<LE>()? as usize;
+        let moments = (0..moment_count)
+            .map(|_| {
+                let moment_index = reader.read_u32::<LE>()?;
+                read_version!(reader, name, is_gmk, "moment", Gm400)?;
+
+                let action_count = reader.read_u32::<LE>()? as usize;
+                let actions = (0..action_count)
+                    .map(|_| Action::read_for(&mut reader, is_gmk, &name, "action in timeline"))
+                    .collect::<Result<_, io::Error>>()?;
+
+                Ok((moment_index, actions))
+            })
+            .collect::<Result<_, io::Error>>()?;
+
+        Ok(Self { name, timestamp, version, moments })
+    }
+
+    fn write(&self, mut writer: &mut dyn io::Write, is_gmk: bool) -> io::Result<()> {
+        assert_eq!(self.version, Version::Gm500);
+        self.name.write(&mut writer)?;
+        if is_gmk {
+            writer.write_f64::<LE>(self.timestamp.0)?;
+        }
+        writer.write_u32::<LE>(self.version as u32)?;
+        
+        writer.write_u32::<LE>(self.moments.len() as u32)?;
+        for (moment_index, actions) in &self.moments {
+            writer.write_u32::<LE>(*moment_index)?;
+            writer.write_u32::<LE>(400)?;
+            writer.write_u32::<LE>(actions.len() as u32)?;
+            for action in actions {
+                action.write(&mut writer)?;
+            }
+        }
+        Ok(())
+    }
+}
