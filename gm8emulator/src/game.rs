437e25f4dfac98167e75cb42f35021a47fbdf908
Adam <classygopher@gmail.com>
2021-06-24 04:16:41 +0100

    game: grid
    grid

--- gm8emulator/src/game.rs
+++ gm8emulator/src/game.rs
@@ -1935,7 +1935,7 @@ impl Game {
 
         let mut replay = Replay::new(self.spoofed_time_nanos.unwrap_or(0), self.rand.seed());
 
-        let clear_colour = Colour::new(0.961, 0.259, 0.925);
+        let clear_colour = Colour::new(0.0196, 0.1059, 0.06275);
 
         let mut context = imgui::Context::new();
         context.make_current();
@@ -1946,6 +1946,22 @@ impl Game {
         let imgui::FontData { data: fdata, size: (fwidth, fheight) } = io.font_data();
         let mut font = self.renderer.upload_sprite(fdata.into(), fwidth as _, fheight as _, 0, 0)?;
         io.set_texture_id((&mut font as *mut AtlasRef).cast());
+
+        let grid = (0i32..(64 * 64 * 4)).map(|i| {
+            let x = (i / 4) % 64;
+            let y = (i / 4) / 64;
+            if (y - x).abs() == 32 || (y + x - 63).abs() == 32 {
+                [0, 192, 99, 255][i as usize % 4]
+            } else if (y >= 34 && x + y == 97) || ((2..32).contains(&y) && x + y == 33) {
+                [64, 64, 64, 255][i as usize % 4]
+            } else if (31..34).contains(&(y - x).abs()) || (31..34).contains(&(y + x - 63).abs()) {
+                [0, 64, 33, 255][i as usize % 4]
+            } else {
+                0
+            }
+        }).collect::<Vec<_>>().into_boxed_slice();
+        let grid_ref = self.renderer.upload_sprite(grid, 64, 64, 0, 0)?;
+        let grid_start = std::time::Instant::now();
 
         // for imgui callback
         struct GameViewData {
@@ -2115,6 +2131,13 @@ impl Game {
             frame.render();
 
             // draw imgui
+            let start_xy = -f64::from(((grid_start.elapsed().as_millis() >> 5) & 0x3F) as i8) - 0.5;
+            for y in (0..(ui_height / 64 + 2)).map(f64::from) {
+                for x in (0..(ui_width / 64 + 2)).map(f64::from) {
+                    self.renderer.draw_sprite(&grid_ref, start_xy + (x * 64.0), start_xy + (y * 64.0), 1.0, 1.0, 0.0, 0xFFFFFF, 0.5);
+                }
+            }
+
             let draw_data = context.draw_data();
             debug_assert!(draw_data.Valid);
             let cmd_list_count = usize::try_from(draw_data.CmdListsCount)?;
