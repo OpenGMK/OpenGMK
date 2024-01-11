d97a4788a4b5269c80b55617026b81479feb6c28
Daggerfall-Is-The-Best-TES-Game <Davidcfried@gmail.com>
2024-01-10 21:47:04 -0500

    basic object filtering

--- hitbox/src/main.rs
+++ hitbox/src/main.rs
@@ -1,4 +1,6 @@
+use gm8exe::asset::PascalString;
 use gm8exe::{asset::Object, GameAssets};
+use std::collections::HashSet;
 use std::str;
 use std::{fs, process};
 
@@ -14,12 +16,35 @@ fn get_assets() -> GameAssets {
     });
 }
 
-fn get_sprite_name(assets: GameAssets, spr_idx: usize) -> String {
-    return str::from_utf8(assets.sprites[spr_idx as usize].as_ref().unwrap().name.0.as_ref()).unwrap().to_string();
+fn get_sprite_name(assets: &GameAssets, spr_idx: usize) -> String {
+    return pascal_string_to_string(&assets.sprites[spr_idx as usize].as_ref().unwrap().name).to_string();
+}
+
+fn pascal_string_to_string(s: &PascalString) -> &str {
+    return str::from_utf8(&s.0.as_ref()).unwrap();
 }
 
 fn main() {
+    let mut bruteforcer_objects: HashSet<&str> = HashSet::<&str>::new();
+    bruteforcer_objects.insert("spikeUp");
+    bruteforcer_objects.insert("spikeDown");
+    bruteforcer_objects.insert("spikeLeft");
+    bruteforcer_objects.insert("spikeRight");
+
     let assets = get_assets();
-    let spr_idx: usize = assets.objects[0].as_ref().unwrap().sprite_index as usize;
-    println!("{:?}", get_sprite_name(assets, spr_idx));
+    for obj in &assets.objects {
+        let object_name = obj.as_ref().map_or("no object", |o| pascal_string_to_string(&o.name));
+        if bruteforcer_objects.contains(object_name) {
+            println!("object name is {}", object_name);
+            let spr_idx: usize = obj
+                .as_ref()
+                .unwrap_or(&assets.objects[0].as_ref().unwrap())
+                .sprite_index
+                .try_into()
+                .unwrap_or_default();
+            println!("sprite index is {}", spr_idx);
+            println!("sprite name is {}", get_sprite_name(&assets, spr_idx));
+            println!("");
+        }
+    }
 }
