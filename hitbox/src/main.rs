631f1de9a1050a34cff35b4690e95bee294a51a5
Daggerfall-Is-The-Best-TES-Game <Davidcfried@gmail.com>
2024-01-12 05:57:16 -0500

    simplify filtering of missing objects

--- hitbox/src/main.rs
+++ hitbox/src/main.rs
@@ -1,6 +1,6 @@
 use gm8exe::asset::sprite::CollisionMap;
 use gm8exe::asset::PascalString;
-use gm8exe::{asset::Object, GameAssets};
+use gm8exe::GameAssets;
 use std::collections::{HashMap, HashSet};
 use std::str;
 use std::{fs, process};
@@ -47,33 +47,30 @@ fn main() {
     let mut game_object_map: HashMap<&str, Vec<&str>> = HashMap::<&str, Vec<&str>>::new();
 
     let assets = get_assets();
-    for obj in &assets.objects {
-        if let Some(obj) = obj {
-            let object_name = pascal_string_to_string(&obj.name);
-            if bruteforcer_objects.contains(object_name) {
-                let spr_idx: usize = obj.sprite_index.try_into().unwrap_or_default();
-                let collider = get_collider(&assets, spr_idx);
-                bruteforcer_object_map.insert((&collider.data, obj.solid), object_name);
-                let mask_idx: usize = obj.mask_index.try_into().unwrap_or_default();
-
-                println!("object name is {}", object_name);
-                println!("sprite index is {}", spr_idx);
-                println!("mask index is {}", mask_idx);
-                println!("sprite name is {}", get_sprite_name(&assets, spr_idx));
-                println!("collider is {:?}", collider.data);
-                println!("");
-            }
-        }
-    }
-    for obj in &assets.objects {
-        if let Some(obj) = obj {
-            let object_name = pascal_string_to_string(&obj.name);
+    assets.objects.iter().flatten().for_each(|obj| {
+        let object_name = pascal_string_to_string(&obj.name);
+        if bruteforcer_objects.contains(object_name) {
             let spr_idx: usize = obj.sprite_index.try_into().unwrap_or_default();
             let collider = get_collider(&assets, spr_idx);
-            if let Some(alias) = bruteforcer_object_map.get(&(&collider.data, obj.solid)) {
-                game_object_map.entry(alias).and_modify(|list| list.push(object_name)).or_insert(vec![object_name]);
-            }
+            bruteforcer_object_map.insert((&collider.data, obj.solid), object_name);
+            let mask_idx: usize = obj.mask_index.try_into().unwrap_or_default();
+
+            println!("object name is {}", object_name);
+            println!("sprite index is {}", spr_idx);
+            println!("mask index is {}", mask_idx);
+            println!("sprite name is {}", get_sprite_name(&assets, spr_idx));
+            println!("collider is {:?}", collider.data);
+            println!("");
         }
-    }
+    });
+    assets.objects.iter().flatten().for_each(|obj| {
+        let object_name = pascal_string_to_string(&obj.name);
+        let spr_idx: usize = obj.sprite_index.try_into().unwrap_or_default();
+        let collider = get_collider(&assets, spr_idx);
+        if let Some(alias) = bruteforcer_object_map.get(&(&collider.data, obj.solid)) {
+            game_object_map.entry(alias).and_modify(|list| list.push(object_name)).or_insert(vec![object_name]);
+        }
+    });
+
     println!("the bruteforcer objects in game {:?}", game_object_map);
 }
